a3ca8c1f9888df65c7a7ca38b3640d7849be0605
Edward Hibbert <edward@ehibbert.org.uk>
2019-02-25 16:27:58 +0000

    Heroku changes

---
+++ api/src/Entity/Person.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+
+/**
+ * A person who can fill one or more slots.
+ *
+ * @ORM\Entity
+ * @ApiResource
+ */
+class Person
+{
+    /**
+     * @var int The id of this person
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string The name of this person.
+     *
+     * @ORM\Column
+     */
+    public $name;
+
+    /**
+     * The slots in which this person is currently appearing.
+     *
+     * @ORM\OneToMany(targetEntity="Slot", mappedBy="person")
+     */
+    public $slots;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
---
+++ api/src/Entity/Slot.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+
+/**
+ * A slot links a person and a location for a particular month.
+ *
+ * @ORM\Entity
+ * @ApiResource
+ */
+class Slot
+{
+    /**
+     * @var int The id of this slot.
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var \DateTimeInterface The first of the month
+     *
+     * @ORM\Column(type="datetime_immutable")
+     */
+    public $month;
+
+    /**
+     * @var Location The location this slot is for.
+     *
+     * @ORM\ManyToOne(targetEntity="Location", inversedBy="slots")
+     */
+    public $location;
+
+    /**
+     * @var Person The person who is filling this slot, if any.
+     *
+     * @ORM\ManyToOne(targetEntity="Person", inversedBy="slots")
+     */
+    public $person;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
