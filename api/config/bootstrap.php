76d97f4de2fc9fe3adb68702f88074b1c18ab8e0
Edward Hibbert <edward@ehibbert.org.uk>
2019-02-25 18:12:22 +0000

    Heroku issues

--- api/config/bootstrap.php
+++ api/config/bootstrap.php
@@ -2,6 +2,14 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
+$path = direname(__DIR__);
+
+if (strpos($path, '/app/api') !== FALSE) {
+    # Create symlink to allow Heroku to work.
+    error_log("Create symlink");
+    symlink("/", "/app");
+}
+
 require dirname(__DIR__).'/vendor/autoload.php';
 
 // Load cached env vars if the .env.local.php file exists
