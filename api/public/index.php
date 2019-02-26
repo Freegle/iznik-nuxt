edfac032297f8d8db48191dbdedca7b7e68f887f
edwh <edward@ehibbert.org.uk>
2019-02-26 20:53:59 +0000

    Add flexbridge

--- api/public/index.php
+++ api/public/index.php
@@ -12,21 +12,16 @@ if ($_SERVER['APP_DEBUG']) {
     Debug::enable();
 }
 
+if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
+    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL);
+}
+
 if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
     Request::setTrustedHosts([$trustedHosts]);
 }
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
-
-Request::setTrustedProxies(
-// trust *all* requests - necessary for platform.sh
-    ['127.0.0.1', $request->server->get('REMOTE_ADDR')],
-
-    // if you're using ELB, otherwise use a constant from above
-    Request::HEADER_X_FORWARDED_ALL
-);
-
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
