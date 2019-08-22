89106c336ddebc675f9e6d6428d9f5932c2054c2
Edward Hibbert <edward@ehibbert.org.uk>
2019-08-22 10:43:34 +0100

    Add ecosystem for pm2 deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps : [
+    {
+      name: "nuxt-prod",
+      script: "npm",
+      args: "run start",
+      instances: 0,
+      autorestart: true,
+      xp_backoff_restart_delay: 100,
+      watch: false,
+      max_memory_restart: '1G',
+    }
+  ],
+
+  deploy : {
+    production : {
+      user : 'root',
+      key  : '/root/.ssh/id_rsa',
+      host : ['46.43.9.246', '5.28.62.22'],
+      ref  : 'origin/master',
+      repo : 'git@github.com:Freegle/iznik-nuxt.git',
+      path : '/var/www/fdnuxt',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
