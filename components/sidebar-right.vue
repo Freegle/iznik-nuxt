f0418e745ba0971dba3c82365c3826b08ac4013f
Jason Harris <jayyaj@hotmail.com>
2019-11-01 13:41:39 +0000

    Create sidebar components

---
+++ components/sidebar-right.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="d-flex flex-column sidebar__wrapper">
+    <VolunteerOpportunitySidebar class="justify-content-start flex-grow-1" style="overflow-y: auto" />
+    Job ads go here
+  </div>
+</template>
+
+<script>
+const VolunteerOpportunitySidebar = () =>
+  import('~/components/VolunteerOpportunitySidebar')
+
+export default {
+  components: {
+    VolunteerOpportunitySidebar
+  }
+}
+</script>
+
+<style scoped>
+.sidebar__wrapper {
+  height: calc(100vh - 100px);
+}
+</style>
