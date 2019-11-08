28401ee16e23b1f038bd1be444460eb3c97933cf
Nick Sellen <git@nicksellen.co.uk>
2019-11-08 11:45:57 +0100

    Document and refine grouproute mixin a bit further

--- mixins/createGroupRouteMixin.js
+++ mixins/createGroupRouteMixin.js
@@ -2,9 +2,21 @@
  * A function to create a mixin to be used where there is an
  * optional group param in the route.
  *
+ * It will provide you a "groupid" property on your vue instance so you MUST
+ * NOT also define that yourself (e.g. in the data function).
+ *
+ * For example when you first visit /communityevents page it shows you events
+ * from all your groups. You can select a group from the select and you will get
+ * redirected to /communityevents/:id. We will save that :id.
+ *
+ * When you next visit /communityevents we look up the remembered :id and redirect
+ * you to that page using "replace state" routing (so it doesn't go into the back
+ * history).
+ *
  * Designed to be used with the GroupSelect component.
  *
  * @param key used for the remember key and the route segment
+ * @param options pass in routeParam to use a different route param key, defaults to id
  * @returns a Vue mixin
  */
 export default function createGroupRouteMixin(key, options = {}) {
@@ -36,19 +48,13 @@ export default function createGroupRouteMixin(key, options = {}) {
         }
       }
     },
-    beforeDestroy() {
-      if (this._unwatchGroupRemember) this._unwatchGroupRemember()
-    },
     created() {
       this._unwatchGroupRemember = this.$store.watch(
         (state, getters) => getters['group/remembered'](rememberId),
         (val, oldVal) => {
           if (oldVal === undefined && val === undefined && this.groupid !== 0) {
             // Nothing set so far... make it what our current page is
-            this.$store.commit('group/remember', {
-              id: rememberId,
-              val: this.groupid
-            })
+            this.rememberGroup()
           } else if (val !== undefined) {
             if (this.groupid === 0) {
               // We have a remember value, but we're on the general page
@@ -56,10 +62,7 @@ export default function createGroupRouteMixin(key, options = {}) {
               this.$router.replace(routePath(val))
             } else if (this.groupid !== val) {
               // We've set the groupid to something else now, save it
-              this.$store.commit('group/remember', {
-                id: rememberId,
-                val: this.groupid
-              })
+              this.rememberGroup()
             }
           }
         },
@@ -67,6 +70,17 @@ export default function createGroupRouteMixin(key, options = {}) {
           immediate: true
         }
       )
+    },
+    methods: {
+      rememberGroup() {
+        this.$store.commit('group/remember', {
+          id: rememberId,
+          val: this.groupid
+        })
+      }
+    },
+    beforeDestroy() {
+      if (this._unwatchGroupRemember) this._unwatchGroupRemember()
     }
   }
 }
