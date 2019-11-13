32b552861520bcb2b960b3bea8de2acfdf3d0651
Nick Sellen <git@nicksellen.co.uk>
2019-11-13 13:18:04 +0100

    Remove array element by identity

--- components/StartEndCollection.vue
+++ components/StartEndCollection.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div v-for="(date, idx) in value" :key="date.uniqueid" :class="date.string && date.string.past ? 'inpast': ''">
-      <StartEndDate v-model="value[idx]" @remove="remove(idx)" />
+      <StartEndDate v-model="value[idx]" @remove="remove(date)" />
     </div>
     <b-btn variant="white" class="mt-1" @click="add">
       <v-icon name="plus" /> Add another date
@@ -44,8 +44,9 @@ export default {
     }
   },
   methods: {
-    remove(idx) {
-      this.value.splice(idx, 1)
+    remove(item) {
+      const idx = this.value.indexOf(item)
+      if (idx !== -1) this.value.splice(idx, 1)
     },
     async add() {
       this.value.push({
