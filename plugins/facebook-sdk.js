2491d56b1c3683b67c9f4cfe9953d8ab7cae832e
Edward Hibbert <edward@ehibbert.org.uk>
2019-11-13 14:13:56 +0000

    Work on privacy blocker handling for social signin

--- plugins/facebook-sdk.js
+++ plugins/facebook-sdk.js
@@ -5,24 +5,34 @@ import Vue from 'vue'
 const VueFB = {}
 
 VueFB.install = function install(Vue, options) {
+  console.log('Load Facebook sdk')
+  Vue.FB = undefined
   ;(function(d, s, id) {
-    const fjs = d.getElementsByTagName(s)[0]
-    if (d.getElementById(id)) {
-      return
+    try {
+      console.log('Install with', d, s, id)
+      const fjs = d.getElementsByTagName(s)[0]
+      if (d.getElementById(id)) {
+        console.log('Already there')
+        return
+      }
+
+      console.log('Not there, add it')
+      const js = d.createElement(s)
+      js.id = id
+      js.src = '//connect.facebook.net/en_US/sdk.js'
+      fjs.parentNode.insertBefore(js, fjs)
+    } catch (e) {
+      console.error('Failed to load Facebook SDK', e)
     }
-    const js = d.createElement(s)
-    js.id = id
-    js.src = '//connect.facebook.net/en_US/sdk.js'
-    fjs.parentNode.insertBefore(js, fjs)
   })(document, 'script', 'facebook-jssdk')
 
   window.fbAsyncInit = function onSDKInit() {
+    console.log('Loaded FB')
     window.FB.init(options)
     window.FB.AppEvents.logPageView()
     Vue.FB = window.FB
     window.dispatchEvent(new Event('fb-sdk-ready'))
   }
-  Vue.FB = undefined
 }
 
 Vue.use(VueFB, {
--- plugins/google-sdk.js
+++ plugins/google-sdk.js
@@ -17,24 +17,24 @@ VueGoogle.install = function install(Vue, options) {
     js.onload = e => {
       setTimeout(() => {
         if (window.gapi) {
-          window.gapi.load('client', {
-            callback: function() {
-              window.gapi.client.init({
-                apiKey: process.env.GOOGLE_API_KEY
-              })
-              window.gapiLoaded = true
-            },
-            onerror: function() {
-              console.error('gapi.client failed to load!')
-            },
-            timeout: 30000,
-            ontimeout: function() {
-              console.error('GAPI client load timed out')
-            }
-          })
-
-          window.gapi.load('auth2')
           try {
+            window.gapi.load('client', {
+              callback: function() {
+                window.gapi.client.init({
+                  apiKey: process.env.GOOGLE_API_KEY
+                })
+                window.gapiLoaded = true
+              },
+              onerror: function() {
+                console.error('gapi.client failed to load!')
+              },
+              timeout: 30000,
+              ontimeout: function() {
+                console.error('GAPI client load timed out')
+              }
+            })
+
+            window.gapi.load('auth2')
           } catch (e) {
             console.error('GAPI load failed', e)
           }
