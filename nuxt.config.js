8516663cf69672b176b2351d630e23dc830ce612
Nick Sellen <git@nicksellen.co.uk>
2019-11-15 13:01:21 +0100

    Set main page title to Freegle

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Freegle',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
