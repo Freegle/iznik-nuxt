43531011ed7f05538d616fffa75520870ba2a930
edwh <edward@ehibbert.org.uk>
2019-11-20 12:18:45 +0000

    Fix getter error

--- mixins/loginOptional.js
+++ mixins/loginOptional.js
@@ -5,7 +5,7 @@ export default {
     // in or not, and for our purposes we only really care about SSR for logged out pages, and therefore we don't need to
     // render the page correctly if they are in fact on the client.  So we can live with that.
     if (!process.server) {
-      if (this.$store.getters['auth/forceLogin']() !== false) {
+      if (this.$store.getters['auth/forceLogin'] !== false) {
         this.$store.dispatch('auth/forceLogin', false)
       }
 
