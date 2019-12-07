53b96f4cd836abfe656c626450d9e9b8effbf617
edwh <edward@ehibbert.org.uk>
2019-12-07 10:00:02 +0000

    When we become active, poll for notification and chat counts.

--- plugins/visibility.js
+++ plugins/visibility.js
@@ -5,8 +5,10 @@ export default ({ store }) => {
     () => visibility.visible,
     visible => {
       if (visible) {
+        // We have become visible.  Refetch our notification count and chat count, which are the two key things which
+        // produce red badges people should click on.
         store.dispatch('notifications/count')
-        store.dispatch('notifications/list')
+        store.dispatch('chats/listChats')
       }
     }
   )
