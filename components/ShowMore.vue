64b3f0164b5c9240802df18b3bf420d020570367
Edward Hibbert <edward@ehibbert.org.uk>
2019-12-14 15:27:44 +0000

    Add ShowMore to allow us to display long lists without using too much screen space.

---
+++ components/ShowMore.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div v-for="item in itemsToShow" :key="item[keyfield]">
+      <slot name="item" :item="item">
+        Item {{ item[keyfield] }}
+      </slot>
+    </div>
+    <b-btn v-if="this.items.length > this.limit && !this.expanded" variant="link" @click="expanded = true">
+      Show more...
+    </b-btn>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true
+    },
+    limit: {
+      type: Number,
+      required: false,
+      default: 10
+    },
+    keyfield: {
+      type: String,
+      required: false,
+      default: 'id'
+    }
+  },
+  data: function() {
+    return {
+      expanded: false
+    }
+  },
+  computed: {
+    itemsToShow() {
+      if (this.expanded || this.items.length < this.limit) {
+        return this.items
+      } else {
+        return this.items.slice(0, this.limit)
+      }
+    }
+  }
+}
+</script>
