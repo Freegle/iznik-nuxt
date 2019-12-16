2ab2a7692827901e11dddc9bc503bb8820fa7fed
Edward Hibbert <edward@ehibbert.org.uk>
2019-12-16 10:11:46 +0000

    Change chitchat to use computed newsfeed rather than having a data property

--- pages/chitchat/_id.vue
+++ pages/chitchat/_id.vue
@@ -174,7 +174,6 @@ export default {
 
   data: function() {
     return {
-      newsfeed: null,
       busy: false,
       startThread: null,
       scrollTo: null,
@@ -245,6 +244,9 @@ export default {
     users() {
       const users = this.$store.getters['newsfeed/users']
       return users
+    },
+    newsfeed() {
+      return this.$store.getters['newsfeed/newsfeed']
     }
   },
 
@@ -328,6 +330,7 @@ export default {
     },
 
     async loadMore($state) {
+      console.log('load more')
       this.busy = true
       const user = this.$store.getters['auth/user']
 
@@ -341,28 +344,22 @@ export default {
 
           if (this.id) {
             // Just one - fetch it by id.
-            this.newsfeed = [
-              await this.$store.dispatch('newsfeed/fetch', {
-                id: this.id
-              })
-            ]
+            await this.$store.dispatch('newsfeed/fetch', {
+              id: this.id
+            })
 
             // But maybe this isn't the thread head.
             const fetched = this.$store.getters['newsfeed/get'](this.id)
             if (fetched.threadhead && this.id !== fetched.threadhead) {
-              this.newsfeed = []
-              this.newsfeed = [
-                await this.$store.dispatch('newsfeed/fetch', {
-                  id: fetched.threadhead
-                })
-              ]
+              await this.$store.dispatch('newsfeed/clearFeed')
+              await this.$store.dispatch('newsfeed/fetch', {
+                id: fetched.threadhead
+              })
             } else if (fetched.replyto && this.id !== fetched.replyto) {
-              this.newsfeed = []
-              this.newsfeed = [
-                await this.$store.dispatch('newsfeed/fetch', {
-                  id: fetched.replyto
-                })
-              ]
+              await this.$store.dispatch('newsfeed/clearFeed')
+              await this.$store.dispatch('newsfeed/fetch', {
+                id: fetched.replyto
+              })
             }
 
             this.scrollTo = this.id
@@ -370,13 +367,12 @@ export default {
             $state.complete()
           } else {
             // Fetch for the area we are interested in.
+            console.log('Fetch')
             await this.$store.dispatch('newsfeed/fetchFeed', {
               context: context,
               distance: this.selectedArea
             })
 
-            // One need this one entry.
-            this.newsfeed = this.$store.getters['newsfeed/newsfeed']
             if ($state.loaded) {
               $state.loaded()
             }
@@ -395,7 +391,6 @@ export default {
     areaChange: function() {
       this.infiniteId++
       this.$store.commit('newsfeed/clearFeed')
-      this.newsfeed = null
     },
     async postIt() {
       let msg = this.startThread
@@ -408,9 +403,6 @@ export default {
           message: msg,
           imageid: this.imageid
         })
-
-        // Show the new message
-        this.newsfeed = this.$store.getters['newsfeed/newsfeed']
 
         // Clear the textarea now it's sent.
         this.startThread = null
