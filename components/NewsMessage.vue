8cddddea5d30a8c9683972af07745849201243be
Jason Harris <jayyaj@hotmail.com>
2019-12-20 14:51:39 +0000

    Refactor some rows/cols in NewsMessage

--- components/NewsMessage.vue
+++ components/NewsMessage.vue
@@ -3,27 +3,22 @@
     <NewsUserIntro v-if="userid" :userid="userid" :users="users" :newsfeed="newsfeed" />
     <read-more v-if="newsfeed.message && emessage" :text="emessage" :max-chars="1024" class="font-weight-bold prewrap forcebreak nopara" />
     <div>
-      <b-row v-if="newsfeed.image">
-        <b-col>
-          <b-img
-            v-b-modal="'photoModal-' + newsfeed.id"
-            thumbnail
-            rounded
-            lazy
-            :src="newsfeed.image.paththumb"
-            class="clickme"
-          />
-        </b-col>
-      </b-row>
+      <b-img
+        v-if="newsfeed.image"
+        v-b-modal="'photoModal-' + newsfeed.id"
+        thumbnail
+        rounded
+        lazy
+        :src="newsfeed.image.paththumb"
+        class="clickme"
+      />
     </div>
-    <b-row class="mt-2">
-      <b-col>
-        <NewsLoveComment :newsfeed="newsfeed" @focus-comment="$emit('focus-comment')" />
-        <b-btn variant="white" size="sm" class="float-right d-inline-block" @click="share">
-          <v-icon name="share-alt" /> Share
-        </b-btn>
-      </b-col>
-    </b-row>
+    <div class="mt-2">
+      <NewsLoveComment :newsfeed="newsfeed" @focus-comment="$emit('focus-comment')" />
+      <b-btn variant="white" size="sm" class="float-right d-inline-block" @click="share">
+        <v-icon name="share-alt" /> Share
+      </b-btn>
+    </div>
     <b-modal
       v-if="newsfeed.image"
       :id="'photoModal-' + newsfeed.id"
