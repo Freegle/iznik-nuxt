e07b1832c527547c07c004a0ea0beb72fa545b00
Edward Hibbert <edward@ehibbert.org.uk>
2019-12-23 16:22:45 +0000

    Stop verbose core-js build messages

--- nuxt.config.js
+++ nuxt.config.js
@@ -341,8 +341,8 @@ module.exports = {
             require.resolve('@nuxt/babel-preset-app'),
             {
               targets,
-              corejs: 3,
-              debug: process.env.NODE_ENV === 'production'
+              corejs: 3
+              // debug: process.env.NODE_ENV === 'production'
             }
           ]
         ]
