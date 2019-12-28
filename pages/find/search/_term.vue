892d4b67e426e92a7e388a80e999d2765ee8894a
Aaron Ferris <aaron.ferris@capgemini.com>
2019-12-28 13:06:15 +0000

    Remove tablet restrictions on columns for the find process

--- pages/find/search/_term.vue
+++ pages/find/search/_term.vue
@@ -2,7 +2,7 @@
   <div>
     <b-row class="m-0">
       <b-col cols="0" md="3" />
-      <b-col cols="12" md="6" class="p-0">
+      <b-col cols="12" lg="6" class="p-0">
         <h1 class="text-center">
           Ok, what are you looking for?
         </h1>
