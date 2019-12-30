f35e2ceed8f8ba5f323793dab046d8730b65119a
Edward Hibbert <edward@ehibbert.org.uk>
2019-12-30 22:08:45 +0000

    The case problem that broke the camel's back

--- components/CommunityEventModal.vue
+++ components/CommunityEventModal.vue
@@ -224,7 +224,7 @@
             <b-form-invalid-feedback class="mb-3">
               Please add at least one date
             </b-form-invalid-feedback>
-            <StartEndCollection v-if="eventEdit.dates" v-model="eventEdit.dates" add-date-if-empty required />
+            <StartEndCollection v-if="eventEdit.dates" v-model="eventEdit.dates" required />
           </b-form-group>
           <b-form-group
             ref="eventEdit__contactname"
