2971e04a31899999f45c61ae34ed3fda7c40c1b4
Edward Hibbert <edward@ehibbert.org.uk>
2020-01-09 12:16:56 +0000

    Add notice if job ads are blocked

--- components/JobsTopBar.vue
+++ components/JobsTopBar.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="mb-2 jobbox bg-light overflow-hidden">
-    <div class="mb-1 text-center small text-muted">
-      Jobs near you.  Freegle gets a small amount if you click, which helps keep us going.  <nuxt-link to="/jobs">
-        See more
-      </nuxt-link>.
-    </div>
-    <div v-for="job in jobs" :key="'job-' + job.onmousedown" class="">
-      <Job :summary="true" :job="job" />
+    <NoticeMessage v-if="blocked" variant="warning">
+      <h3>Please help keep Freegle running</h3>
+      <p>
+        We normally show job ads here.  It looks like you have an AdBlocker or security software which is blocking those.
+        We're not mad on ads either, but please consider donating to help us keep going:
+      </p>
+      <a href="https://freegle.in/paypalfundraiser" target="_blank">
+        <b-btn variant="primary" size="lg">
+          <b-img thumbnail alt="PayPal" src="https://www.paypalobjects.com/webstatic/mktg/logo/pp_cc_mark_37x23.jpg" />
+          &nbsp;Donate
+        </b-btn>
+      </a>
+    </NoticeMessage>
+    <div v-else>
+      <div class="mb-1 text-center small text-muted">
+        Jobs near you.  Freegle gets a small amount if you click, which helps keep us going.  <nuxt-link to="/jobs">
+          See more
+        </nuxt-link>.
+      </div>
+      <div v-for="job in jobs" :key="'job-' + job.onmousedown" class="">
+        <Job :summary="true" :job="job" />
+      </div>
     </div>
   </div>
 </template>
@@ -17,9 +32,11 @@
 </style>
 <script>
 import Job from './Job'
+import NoticeMessage from './NoticeMessage'
 
 export default {
   components: {
+    NoticeMessage,
     Job
   },
   data: function() {
@@ -30,6 +47,9 @@ export default {
       let jobs = this.$store.getters['jobs/list']
       jobs = jobs.slice(0, 3)
       return jobs
+    },
+    blocked() {
+      return this.$store.getters['jobs/blocked']
     }
   },
   mounted() {
