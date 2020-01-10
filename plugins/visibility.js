2b6ce3a12a645d6ee0b55b264824a73a0a4b2dd6
Edward Hibbert <edward@ehibbert.org.uk>
2020-01-10 23:00:08 +0000

    Don't poll for notifications and chat messages when we become visible if we are not logged in, because they only apply to logged-in users.

--- plugins/visibility.js
+++ plugins/visibility.js
@@ -4,7 +4,9 @@ export default ({ store }) => {
   store.watch(
     () => visibility.visible,
     visible => {
-      if (visible) {
+      const me = store.getters['auth/user']
+
+      if (me && visible) {
         // We have become visible.  Refetch our notification count and chat count, which are the two key things which
         // produce red badges people should click on.
         store.dispatch('notifications/count')
