46764b9b59b2bafb937980975d58aa45824be5ea
chriscant <chris@phdcc.com>
2020-01-12 13:46:40 +0000

    Inc mobile version

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import sitemap from './utils/sitemap.js'
 
 const FACEBOOK_APPID = '134980666550322'
 const SENTRY_DSN = 'https://4de62393d60a4d2aae4ccc3519e94878@sentry.io/1868170'
-const MOBILE_VERSION = '2.0.1'
+const MOBILE_VERSION = '2.0.2'
 
 require('dotenv').config()
 
