bbc86aa2f649c4fc3733be4af404a195082d6740
Edward Hibbert <edward@ehibbert.org.uk>
2020-01-16 19:59:02 +0000

    Suggest refresh in "stuck" message.

--- layouts/default.vue
+++ layouts/default.vue
@@ -267,7 +267,8 @@
       <p>
         <b>Loading...</b>
         <br>
-        Stuck here? <a href="mailto:support@ilovefreegle.org">Contact us</a><br>Or try Chrome.
+        Stuck here? <a href="mailto:support@ilovefreegle.org">Contact us</a>
+        <br>Try refreshing.  Or Chrome.
       </p>
     </div>
   </div>
