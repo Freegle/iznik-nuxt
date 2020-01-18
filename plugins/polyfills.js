b26ace300f5e8c43d77411257cca799c84a2ab35
Edward Hibbert <edward@ehibbert.org.uk>
2020-01-18 11:20:18 +0000

    Add a couple of polyfills we may need

--- plugins/polyfills.js
+++ plugins/polyfills.js
@@ -5,3 +5,5 @@ import 'core-js/es/promise'
 
 // We need various polyfills for the Facebook SDK on IE11, and it's getting tedious to work out which.  Grab 'em all.
 import 'core-js/es/array'
+import 'core-js/es/object'
+import 'core-js/es/symbol'
