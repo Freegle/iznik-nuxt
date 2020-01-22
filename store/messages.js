746119dd74f525d669d52c73f67c22f36a2baef1
Edward Hibbert <edward@ehibbert.org.uk>
2020-01-22 21:33:09 +0000

    Withdrawing a pending post causes an error

--- store/messages.js
+++ store/messages.js
@@ -106,7 +106,12 @@ export const actions = {
 
   async update({ commit, dispatch }, params) {
     const data = await this.$api.message.update(params)
-    await dispatch('fetch', { id: params.id })
+
+    if (!data.deleted) {
+      // Fetch back the updated version.
+      await dispatch('fetch', { id: params.id })
+    }
+
     return data
   },
 
