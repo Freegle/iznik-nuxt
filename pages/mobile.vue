9684fc7461f80d0e956cbf81358eb638a9155fc0
Edward Hibbert <edward@ehibbert.org.uk>
2020-01-24 21:14:50 +0000

    Add mobile links to Help

--- pages/mobile.vue
+++ pages/mobile.vue
@@ -16,18 +16,14 @@
         <p>
           Freegling is easy on mobiles and tablets, and you get notifications of replies so you don't have to rely on email. Download using these links or search for Freegle in your app store - it's free!
         </p>
-        <b-row>
-          <b-col>
-            <a href="https://play.google.com/store/apps/details?id=org.ilovefreegle.direct" class="mt-2" target="_blank">
-              <img alt="Freegle Android app on Google Play" title="Freegle Android app on Google Play" class="img-responsive" src="/en-play-badge.png">
-            </a>
-          </b-col>
-          <b-col>
-            <a href="https://itunes.apple.com/gb/app/freegle/id970045029?ls=1&amp;mt=8" class="mt-2" target="_blank">
-              <img alt="Freegle app for iPhone, iPad, and iPod touch" title="Freegle app for iPhone, iPad, and iPod touch" class="img-responsive" src="/app-store-black-sm.png">
-            </a>
-          </b-col>
-        </b-row>
+        <div class="d-flex justify-content-between">
+          <a href="https://play.google.com/store/apps/details?id=org.ilovefreegle.direct" class="mt-2" target="_blank">
+            <img alt="Freegle Android app on Google Play" title="Freegle Android app on Google Play" class="img-responsive" src="/en-play-badge.png">
+          </a>
+          <a href="https://itunes.apple.com/gb/app/freegle/id970045029?ls=1&amp;mt=8" class="mt-2" target="_blank">
+            <img alt="Freegle app for iPhone, iPad, and iPod touch" title="Freegle app for iPhone, iPad, and iPod touch" class="img-responsive" src="/app-store-black-sm.png">
+          </a>
+        </div>
         <p class="mt-2">
           The app is only available in the UK app stores. We support Android 4.4/iOS 9 or later.
         </p>
