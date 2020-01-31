36bbfd70e77133a8524211f1daf1940fa31d04c3
Jason Harris <jayyaj@hotmail.com>
2020-01-31 14:32:19 +0000

    First pass as DRYing out the component

--- components/ChatMessageText.vue
+++ components/ChatMessageText.vue
@@ -2,24 +2,10 @@
   <div>
     <b-row class="pb-1">
       <b-col>
-        <div v-if="chatmessage.userid != $store.state.auth.user.id" class="chatMessageWrapper">
-          <div class="chatMessageProfilePic">
-            <div>
-              <profile-image v-if="othericon" :image="othericon" class="mr-1 mb-1 mt-1 inline" is-thumbnail size="sm" />
-            </div>
-          </div>
-          <div :class="emessage ? 'chatMessage theirs' : ''">
+        <div class="chatMessageWrapper" :class="messageIsFromCurrentUser ? 'myChatMessage' : ''">
+          <div class="chatMessage" :class="messageIsFromCurrentUser ? 'mine' : 'theirs'">
             <span>
-              <span v-if="(chatmessage.secondsago < 60) || (chatmessage.id > chat.lastmsgseen)" class="prewrap"><b>{{ emessage }}</b></span>
-              <span v-else class="prewrap forcebreak">{{ emessage }}</span>
-              <b-img v-if="chatmessage.image" fluid :src="chatmessage.image.path" lazy rounded />
-            </span>
-          </div>
-        </div>
-        <div v-else class="myChatMessage chatMessageWrapper">
-          <div :class="emessage ? 'chatMessage mine' : ''">
-            <span>
-              <span v-if="(chatmessage.secondsago < 60) || (chatmessage.id > chat.lastmsgseen)" class="prewrap"><b>{{ emessage }}</b></span>
+              <span v-if="messageIsNew" class="prewrap"><b>{{ emessage }}</b></span>
               <span v-else class="prewrap forcebreak">{{ emessage }}</span>
               <b-img v-if="chatmessage.image" fluid :src="chatmessage.image.path" lazy rounded />
             </span>
@@ -30,10 +16,63 @@
             </div>
           </div>
         </div>
+        <!--
+        <div v-if="messageIsFromCurrentUser" class="myChatMessage chatMessageWrapper">
+          <div :class="emessage ? 'chatMessage mine' : ''">
+            <span>
+              <span v-if="messageIsNew" class="prewrap"><b>{{ emessage }}</b></span>
+              <span v-else class="prewrap forcebreak">{{ emessage }}</span>
+              <b-img v-if="chatmessage.image" fluid :src="chatmessage.image.path" lazy rounded />
+            </span>
+          </div>
+          <div class="chatMessageProfilePic">
+            <div>
+              <profile-image :image="me.profile.turl" class="ml-1 mb-1 mt-1 inline" is-thumbnail size="sm" />
+            </div>
+          </div>
+        </div>
+        <div v-else class="chatMessageWrapper">
+          <div class="chatMessageProfilePic">
+            <div>
+              <profile-image v-if="othericon" :image="othericon" class="mr-1 mb-1 mt-1 inline" is-thumbnail size="sm" />
+            </div>
+          </div>
+          <div :class="emessage ? 'chatMessage theirs' : ''">
+            <span>
+              <span v-if="messageIsNew" class="prewrap"><b>{{ emessage }}</b></span>
+              <span v-else class="prewrap forcebreak">{{ emessage }}</span>
+              <b-img v-if="chatmessage.image" fluid :src="chatmessage.image.path" lazy rounded />
+            </span>
+          </div>
+        </div>
+      -->
       </b-col>
     </b-row>
   </div>
 </template>
+
+<script>
+import ChatBase from '~/components/ChatBase'
+import ProfileImage from '~/components/ProfileImage'
+
+export default {
+  components: {
+    ProfileImage
+  },
+  extends: ChatBase,
+  computed: {
+    messageIsFromCurrentUser() {
+      return this.chatmessage.userid === this.$store.state.auth.user.id
+    },
+    messageIsNew() {
+      return (
+        this.chatmessage.secondsago < 60 ||
+        this.chatmessage.id > this.chat.lastmsgseen
+      )
+    }
+  }
+}
+</script>
 
 <style scoped lang="scss">
 @import 'color-vars';
@@ -85,21 +124,18 @@
 
 .theirs {
   background-color: $color-white;
+  order: 2;
 }
 
 .mine {
   background-color: $color-green--light;
 }
-</style>
 
-<script>
-import ChatBase from '~/components/ChatBase'
-import ProfileImage from '~/components/ProfileImage'
-
-export default {
-  components: {
-    ProfileImage
-  },
-  extends: ChatBase
+:-ms-fullscreen,
+:root .ie11up {
+  .media {
+    /*On IE11 this stops the chat messages being only a character wide.   */
+    min-width: 40%;
+  }
 }
-</script>
+</style>
