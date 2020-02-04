ba00d34009038e9b8fc223818d88ed715d23c398
Jason Harris <jayyaj@hotmail.com>
2020-02-04 09:34:04 +0000

    Refactor out the duplicated message history section into a component

---
+++ components/MessageHistory.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div v-for="group in groups" :key="'message-' + id + '-' + group.id" class="message-history">
+      {{ group.arrival | timeago }} on <nuxt-link :to="'/explore/' + group.groupid">
+        {{ group.namedisplay }}
+      </nuxt-link>
+      <nuxt-link v-if="displayMessageLink" :to="'/message/' + id" class="text-sm text-faded">
+        #{{ id }}
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MessageHistory',
+  props: {
+    id: {
+      type: Number,
+      default: 0
+    },
+    groups: {
+      type: Array,
+      default: () => []
+    },
+    displayMessageLink: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.message-history {
+  font-size: 0.9rem;
+  font-weight: 400;
+}
+</style>
