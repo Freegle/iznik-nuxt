e4c290ace21aac70a09368ae3d1f6e0589a23bba
chriscant <chris@phdcc.com>
2020-03-04 10:55:42 +0000

    App share from news and story screens

--- components/NewsBase.vue
+++ components/NewsBase.vue
@@ -56,7 +56,32 @@ export default {
     share() {
       this.newsfeedModal = this.newsfeed
       this.$nextTick(() => {
-        this.$bvModal.show('newsShareModal-' + this.newsfeed.id)
+        if (process.env.IS_APP) { // CC..
+          const href = 'https://www.ilovefreegle.org/chitchat/' + this.newsfeed.id + '?src=mobileshare'
+          console.log('NewsBase.vue share', href)
+          const subject = 'Sharing Freegle chitchat'
+          // https://github.com/EddyVerbruggen/SocialSharing-PhoneGap-Plugin
+          const options = {
+            message: this.newsfeed.message + "\n\n",  // not supported on some apps (Facebook, Instagram)
+            subject: 'Sharing Freegle chitchat',      // for email
+            //files: ['', ''], // an array of filenames either locally or remotely
+            url: href,
+            //chooserTitle: 'Pick an app' // Android only, you can override the default share sheet title
+          }
+
+          const onSuccess = function (result) {
+            console.log("Share completed? " + result.completed)   // On Android apps mostly return false even while it's true
+            console.log("Shared to app: " + result.app)           // On Android result.app is currently empty. On iOS it's empty when sharing is cancelled (result.completed=false)
+          }
+
+          const onError = function (msg) {
+            console.log("Sharing failed with message: " + msg)
+          }
+
+          window.plugins.socialsharing.shareWithOptions(options, onSuccess, onError)
+        } else {
+          this.$bvModal.show('newsShareModal-' + this.newsfeed.id)
+        }
       })
     },
     showPhotoModal() {
