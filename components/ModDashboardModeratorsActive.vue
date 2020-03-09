4d05bff8abe4d69abb65725eccc878e10e010d79
Edward Hibbert <edward@ehibbert.org.uk>
2020-03-09 17:43:16 +0000

    MT: Add an indicator to show why active mods isn't showing.

--- components/ModDashboardModeratorsActive.vue
+++ components/ModDashboardModeratorsActive.vue
@@ -1,34 +1,39 @@
 <template>
-  <div v-if="groupid">
+  <div>
     <h2>Active Volunteers</h2>
-    <b-card v-if="loading" no-body>
-      <b-card-body>
-        <b-row><b-col>&nbsp;</b-col></b-row>
-        <b-row>
-          <b-col class="text-faded pulsate text-center">
-            Loading...
-          </b-col>
-        </b-row>
-        <b-row><b-col>&nbsp;</b-col></b-row>
-      </b-card-body>
-    </b-card>
-    <div v-else-if="ModeratorsActive && ModeratorsActive.length">
-      <b-card no-body>
+    <div v-if="groupid">
+      <b-card v-if="loading" no-body>
         <b-card-body>
-          <b-row v-for="user in ModeratorsActive" :key="'popular-' + user.id">
-            <b-col cols="2" class="text-nowrap">
-              <v-icon name="hashtag" scale="0.75" class="text-muted" />{{ user.id }}
-            </b-col>
-            <b-col cols="6" class="text-success font-weight-bold">
-              <ProfileImage :image="user.profile.turl" is-thumbnail size="sm" :is-moderator="user.systemrole === 'Moderator' || user.systemrole === 'Support' || user.systemrole === 'Admin'" />
-              {{ user.displayname }}
-            </b-col>
-            <b-col cols="2">
-              {{ user.lastactive | timeago }}
+          <b-row><b-col>&nbsp;</b-col></b-row>
+          <b-row>
+            <b-col class="text-faded pulsate text-center">
+              Loading...
             </b-col>
           </b-row>
+          <b-row><b-col>&nbsp;</b-col></b-row>
         </b-card-body>
       </b-card>
+      <div v-else-if="ModeratorsActive && ModeratorsActive.length">
+        <b-card no-body>
+          <b-card-body>
+            <b-row v-for="user in ModeratorsActive" :key="'popular-' + user.id">
+              <b-col cols="2" class="text-nowrap">
+                <v-icon name="hashtag" scale="0.75" class="text-muted" />{{ user.id }}
+              </b-col>
+              <b-col cols="6" class="text-success font-weight-bold">
+                <ProfileImage :image="user.profile.turl" is-thumbnail size="sm" :is-moderator="user.systemrole === 'Moderator' || user.systemrole === 'Support' || user.systemrole === 'Admin'" />
+                {{ user.displayname }}
+              </b-col>
+              <b-col cols="2">
+                {{ user.lastactive | timeago }}
+              </b-col>
+            </b-row>
+          </b-card-body>
+        </b-card>
+      </div>
+    </div>
+    <div v-else>
+      <p>Select a community and click <em>Update</em> to show the active moderators.</p>
     </div>
   </div>
 </template>
