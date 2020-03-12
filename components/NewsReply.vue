50a201005674303c806642977cb63e65dd004ce9
Edward Hibbert <edward@ehibbert.org.uk>
2020-03-12 13:01:44 +0000

    "Thread isn't around any more" error when editing a chitchat reply.

--- components/NewsReply.vue
+++ components/NewsReply.vue
@@ -465,7 +465,8 @@ export default {
     save() {
       this.$store.dispatch('newsfeed/edit', {
         id: this.replyid,
-        message: this.reply.message
+        message: this.reply.message,
+        threadhead: this.reply.threadhead
       })
 
       this.$refs.editModal.hide()
