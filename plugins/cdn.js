08f48076e259a8a61919a9d713de6779e655ca69
Edward Hibbert <edward@ehibbert.org.uk>
2020-03-18 05:00:31 +0000

    Disable use of separate CDN

--- plugins/cdn.js
+++ plugins/cdn.js
@@ -11,8 +11,8 @@ if (process.client) {
       break
     case 'dev.ilovefreegle.org':
       // eslint-disable-next-line no-undef,camelcase
-      __webpack_public_path__ =
-        'https://freeglecdndev.azureedge.net' + '/_nuxt/'
+      // __webpack_public_path__ =
+      //   'https://freeglecdndev.azureedge.net' + '/_nuxt/'
       break
     case 'www.ilovefreegle.org':
       // eslint-disable-next-line no-undef,camelcase
