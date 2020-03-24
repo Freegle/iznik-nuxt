42d0c7615edf9484102eee12b9a120ff168c3f7e
Edward Hibbert <edward@ehibbert.org.uk>
2020-03-24 08:54:17 +0000

    Tweak suspension warning

--- components/CovidWarning.vue
+++ components/CovidWarning.vue
@@ -1,5 +1,8 @@
 <template>
-  <NoticeMessage variant="danger">
+  <NoticeMessage v-if="show" variant="danger">
+    <b-btn variant="link" class="float-right" @click="hideit">
+      Hide this
+    </b-btn>
     <p>
       <b>Very sadly, Freegle has suspended OFFER/WANTED posts due to COVID-19.</b>
     </p>
@@ -15,22 +18,49 @@
 
       <DonationButton link="paypal1" show="£1/month" monthly class="mt-2" />
     </p>
-    <p>
-      Our <nuxt-link to="/chitchat">
-        ChitChat section
-      </nuxt-link> is staying open.  Join us here to swap hints,
-      tips and tell others how you’re filling your day.  Share your knowledge and questions on upcycling, reuse,
-      crafts, gardening, cooking, hobbies etc.
-    </p>
+    <div v-if="$route.path !== '/chitchat'">
+      <p>
+        Our <nuxt-link to="/chitchat">
+          ChitChat section
+        </nuxt-link> is staying open.  Join us here to swap hints,
+        tips and tell others how you’re filling your day.  Share your knowledge and questions on upcycling, reuse,
+        crafts, gardening, cooking, hobbies etc.
+      </p>
+      <b-btn to="/chitchat" variant="white" size="lg" class="mb-2">
+        Join us on ChitChat <v-icon name="angle-double-right" />
+      </b-btn>
+    </div>
     <p>
       See you on the other side!
     </p>
   </NoticeMessage>
+  <div v-else class="text-danger text-right clickme" @click="showit">
+    Show COVID-19 warning.
+  </div>
 </template>
 <script>
 import NoticeMessage from './NoticeMessage'
 import DonationButton from './DonationButton'
 export default {
-  components: { DonationButton, NoticeMessage }
+  components: { DonationButton, NoticeMessage },
+  computed: {
+    show() {
+      return !this.$store.getters['misc/get']('hidecovidwarning')
+    }
+  },
+  methods: {
+    hideit() {
+      this.$store.dispatch('misc/set', {
+        key: 'hidecovidwarning',
+        value: true
+      })
+    },
+    showit() {
+      this.$store.dispatch('misc/set', {
+        key: 'hidecovidwarning',
+        value: false
+      })
+    }
+  }
 }
 </script>
