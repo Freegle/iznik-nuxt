b3ed3bffe8c19bee69691a99fd6746fb4668f182
Edward Hibbert <edward@ehibbert.org.uk>
2020-03-25 13:39:49 +0000

    MT: Tweak deleted message log

--- components/ModLogMessage.vue
+++ components/ModLogMessage.vue
@@ -1,12 +1,17 @@
 <template>
-  <span v-if="log.message">
-    <a :href="'https://www.ilovefreegle.org/message/' + log.msgid" target="_blank">
-      <v-icon name="hashtag" class="text-muted" scale="0.75" />{{ log.msgid }}
-      <span v-if="log.message"><em>{{ messagesubject }}</em></span>
-    </a>
-    <span v-if="!notext && log.text && log.text.length > 0"> with <em>{{ log.text }} </em></span>
-    <ModLogStdMsg :log="log" />
-    <ModLogGroup :log="log" :tag="tag" />
+  <span v-if="log.msgid">
+    <span v-if="log.message">
+      <a :href="'https://www.ilovefreegle.org/message/' + log.msgid" target="_blank">
+        <v-icon name="hashtag" class="text-muted" scale="0.75" />{{ log.msgid }}
+        <span v-if="log.message"><em>{{ messagesubject }}</em></span>
+      </a>
+      <span v-if="!notext && log.text && log.text.length > 0"> with <em>{{ log.text }} </em></span>
+      <ModLogStdMsg :log="log" />
+      <ModLogGroup :log="log" :tag="tag" />
+    </span>
+    <span v-else>
+      <v-icon name="hashtag" class="text-muted" scale="0.75" />{{ log.msgid }} (no info available)
+    </span>
   </span>
 </template>
 <script>
