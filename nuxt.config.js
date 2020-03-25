702f53536415c82047cc1e54a48a8c001cdb5517
Edward Hibbert <edward@ehibbert.org.uk>
2020-03-25 13:08:46 +0000

    Ooops, back to universal mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,7 @@ const DISABLE_ESLINT_AUTOFIX =
 const ESLINT_AUTOFIX = !DISABLE_ESLINT_AUTOFIX
 
 module.exports = {
-  mode: 'spa',
+  mode: 'universal',
 
   /*
   ** Headers.  Include default meta tags that will apply unless overridden by individual pages.  Every page that
