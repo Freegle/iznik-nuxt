0d027bf23c5f7bfe43ac768ca0ed0d56a9b4afe1
Edward Hibbert <edward@ehibbert.org.uk>
2020-03-26 17:10:30 +0000

    Reopen for OFFERs

---
+++ components/CovidQueue.vue
@@ -0,0 +1,14 @@
+<template>
+  <NoticeMessage variant="warning">
+    <p class="font-weight-bold">
+      Stuck at home?  Tidying up?  Queue up your OFFERs - once it's safe to freegle again we'll send
+      them out. Don't mope and mutter, just de-clutter!
+    </p>
+  </NoticeMessage>
+</template>
+<script>
+import NoticeMessage from './NoticeMessage'
+export default {
+  components: { NoticeMessage }
+}
+</script>
--- pages/myposts.vue
+++ pages/myposts.vue
@@ -2,13 +2,232 @@
   <b-container fluid>
     <b-row class="m-0">
       <b-col cols="0" lg="3" class="d-none d-lg-block p-0 pr-1">
-        <SidebarLeft :show-community-events="true" :show-bot-left="true" />
+        <!--        COVID-->
+        <SidebarLeft v-if="false" :show-community-events="true" :show-bot-left="true" />
       </b-col>
       <b-col cols="12" lg="6" class="p-0">
-        <CovidWarning />
+        <ExpectedRepliesWarning v-if="me && me.expectedreplies" :count="me.expectedreplies" :chats="me.expectedchats" />
+        <JobsTopBar />
+        <CovidQueue />
+        <b-card
+          v-if="!simple && false"
+          class="mt-2"
+          border-variant="info"
+          header="info"
+          header-bg-variant="info"
+          header-text-variant="white"
+          no-body
+        >
+          <template slot="header">
+            <h3 class="d-inline">
+              <v-icon name="calendar-alt" scale="2" /> Your Availability
+            </h3>
+          </template>
+          <b-card-body>
+            <p>
+              Tell us when you're free and it'll make it quicker to arrange collection times.
+            </p>
+            <b-btn size="lg" variant="success" @click="availability">
+              <v-icon name="calendar-alt" /> Update your availability
+            </b-btn>
+          </b-card-body>
+        </b-card>
+        <b-card
+          class="mt-2"
+          border-variant="info"
+          header="info"
+          header-bg-variant="info"
+          header-text-variant="white"
+          no-body
+        >
+          <template slot="header">
+            <h3 class="d-inline">
+              <v-icon name="gift" scale="2" /> Your queued OFFERs
+            </h3>
+          </template>
+          <b-card-body class="p-1 p-lg-3">
+            <b-card-text class="text-center">
+              <p v-if="offers.length > 0" class="text-muted">
+                These are queued up for when Freegle fully reopens.
+              </p>
+              <b-img-lazy v-if="busy && queued.length === 0" src="~/static/loader.gif" alt="Loading..." />
+              <div v-if="busy || queuedCount > 0">
+                <div v-for="message in queued" :key="'message-' + message.id" class="p-0 text-left mt-1">
+                  <MyMessage :message="message" :messages="messages" :show-old="false" />
+                </div>
+              </div>
+              <div v-else>
+                <b-row>
+                  <b-col>
+                    <p>Nothing here yet.  Why not...</p>
+                  </b-col>
+                </b-row>
+                <b-row>
+                  <b-col class="text-center">
+                    <b-button to="/give" class="mt-1" size="lg" variant="success">
+                      <v-icon name="gift" />&nbsp;OFFER something
+                    </b-button>
+                  </b-col>
+                </b-row>
+              </div>
+            </b-card-text>
+          </b-card-body>
+        </b-card>
+        <b-card
+          class="mt-2"
+          border-variant="info"
+          header="info"
+          header-bg-variant="info"
+          header-text-variant="white"
+          no-body
+        >
+          <template slot="header">
+            <h3 class="d-inline">
+              <v-icon name="gift" scale="2" /> Your OFFERs
+            </h3>
+            <span v-if="oldOfferCount > 0">
+              <span v-if="showOldOffers" class="float-right">
+                <b-btn variant="white" title="Show old OFFERs" @click="toggleOldOffer">
+                  Hide {{ oldOfferCount | pluralize([ 'old OFFER', 'old OFFERs' ], { includeNumber: true }) }}
+                </b-btn>
+              </span>
+              <span v-else class="float-right">
+                <b-btn variant="white" @click="toggleOldOffer">
+                  +{{ oldOfferCount | pluralize([ 'old OFFER', 'old OFFERs' ], { includeNumber: true }) }}
+                </b-btn>
+              </span>
+            </span>
+          </template>
+          <b-card-body class="p-1 p-lg-3">
+            <b-card-text class="text-center">
+              <p v-if="offers.length > 0" class="text-muted">
+                Stuff you're giving away.
+              </p>
+              <b-img-lazy v-if="busy && offers.length === 0" src="~/static/loader.gif" alt="Loading..." />
+              <div v-if="busy || activeOfferCount > 0 || (showOldOffers && offers.length > 0)">
+                <div v-for="message in offers" :key="'message-' + message.id" class="p-0 text-left mt-1">
+                  <MyMessage :message="message" :messages="messages" :show-old="showOldOffers" :expand="expand" />
+                </div>
+              </div>
+              <div v-else>
+                <b-row>
+                  <b-col>
+                    <p>Nothing here yet.  Why not...</p>
+                  </b-col>
+                </b-row>
+                <b-row>
+                  <b-col class="text-center">
+                    <b-button to="/give" class="mt-1" size="lg" variant="success">
+                      <v-icon name="gift" />&nbsp;OFFER something
+                    </b-button>
+                  </b-col>
+                </b-row>
+              </div>
+            </b-card-text>
+          </b-card-body>
+        </b-card>
+        <b-card
+          v-if="false"
+          class="mt-2"
+          border-variant="info"
+          header="info"
+          header-bg-variant="info"
+          header-text-variant="white"
+          no-body
+        >
+          <template slot="header">
+            <h3 class="d-inline">
+              <v-icon name="search" scale="2" /> Your WANTEDs
+            </h3>
+            <span v-if="oldWantedCount > 0">
+              <span v-if="showOldWanteds" class="float-right">
+                <b-btn variant="white" title="Show old WANTEDs" @click="toggleOldWanted">
+                  Hide {{ oldWantedCount | pluralize([ 'old WANTED', 'old WANTEDs' ], { includeNumber: true }) }}
+                </b-btn>
+              </span>
+              <span v-else class="float-right">
+                <b-btn variant="white" @click="toggleOldWanted">
+                  +{{ oldWantedCount | pluralize([ 'old WANTED', 'old WANTEDs' ], { includeNumber: true }) }}
+                </b-btn>
+              </span>
+            </span>
+          </template>
+          <b-card-body class="p-1 p-lg-3">
+            <b-card-text class="text-center">
+              <p v-if="wanteds.length > 0" class="text-muted">
+                Stuff you're trying to find.
+              </p>
+              <div v-if="busy || activeWantedCount > 0 || (showOldWanteds && wanteds.length > 0)">
+                <div v-for="message in wanteds" :key="'message-' + message.id" class="p-0 text-left mt-1">
+                  <MyMessage :message="message" :messages="messages" :show-old="showOldWanteds" :expand="expand" />
+                </div>
+              </div>
+              <div v-else>
+                <p>Nothing here yet.  Why not...</p>
+                <div class="d-flex justify-content-around mb-2">
+                  <b-button to="/find" class="mt-1" size="lg" variant="primary">
+                    <v-icon name="search" />&nbsp;Find stuff
+                  </b-button>
+                  <b-button to="/find/whatisit" class="mt-1" size="lg" variant="info">
+                    <v-icon name="gift" />&nbsp;Post a WANTED
+                  </b-button>
+                </div>
+              </div>
+            </b-card-text>
+          </b-card-body>
+        </b-card>
+        <b-card
+          v-if="!simple && false"
+          class="mt-2"
+          border-variant="info"
+          header="info"
+          header-bg-variant="info"
+          header-text-variant="white"
+          no-body
+        >
+          <template slot="header">
+            <h3 class="d-inline">
+              <v-icon name="search" scale="2" /> Your Searches
+            </h3>
+          </template>
+          <b-card-body class="p-1 p-lg-3">
+            <b-card-text class="text-center">
+              <p v-if="searches.length > 0" class="text-muted">
+                What you've recently searched for - click to search again. These are also email alerts - we'll mail you matching posts.
+              </p>
+              <ul v-if="busy || searches && Object.keys(searches).length > 0" class="list-group list-group-horizontal flex-wrap">
+                <li v-for="search in searches" :key="'search-' + search.id" class="text-left mt-1 list-group-item bg-white border text-nowrap mr-2">
+                  <b-btn :to="'/find/search/' + search.term" variant="white d-inline">
+                    <v-icon name="search" /> {{ search.term }} <span class="text-muted small">{{ search.daysago | pluralize(['day ago', 'days ago'], { includeNumber: true }) }}</span>
+                  </b-btn>
+                  <span class="ml-3 d-inline clickme" @click="deleteSearch(search.id)">
+                    <v-icon v-if="removingSearch === search.id" name="sync" class="text-success fa-spin" />
+                    <v-icon v-else-if="removedSearch === search.id" name="check" class="text-success" />
+                    <v-icon v-else name="trash-alt" title="Delete this search" />
+                  </span>
+                </li>
+              </ul>
+              <div v-else>
+                <b-row>
+                  <b-col>
+                    <p>Nothing here yet.  Why not...</p>
+                  </b-col>
+                </b-row>
+                <b-row>
+                  <b-col class="text-center">
+                    <b-button to="/find" class="mt-1" size="lg" variant="primary">
+                      <v-icon name="search" />&nbsp;Find stuff
+                    </b-button>
+                  </b-col>
+                </b-row>
+              </div>
+            </b-card-text>
+          </b-card-body>
+        </b-card>
       </b-col>
       <b-col cols="0" lg="3" class="d-none d-lg-block p-0 pl-1">
-        <sidebar-right show-volunteer-opportunities />
+        <!--        COVID-->
+        <sidebar-right v-if="false" show-volunteer-opportunities />
       </b-col>
     </b-row>
     <AvailabilityModal v-if="me" ref="availabilitymodal" :thisuid="me.id" />
@@ -17,22 +236,29 @@
 </template>
 
 <script>
-import CovidWarning from '../components/CovidWarning'
+import CovidQueue from '../components/CovidQueue'
 import loginRequired from '@/mixins/loginRequired.js'
 import buildHead from '@/mixins/buildHead.js'
 import waitForRef from '@/mixins/waitForRef'
+const JobsTopBar = () => import('../components/JobsTopBar')
+const MyMessage = () => import('~/components/MyMessage.vue')
 const SidebarLeft = () => import('~/components/SidebarLeft')
 const SidebarRight = () => import('~/components/SidebarRight')
 const AvailabilityModal = () => import('~/components/AvailabilityModal')
 const DonationAskModal = () => import('~/components/DonationAskModal')
+const ExpectedRepliesWarning = () =>
+  import('~/components/ExpectedRepliesWarning')
 
 export default {
   components: {
-    CovidWarning,
+    CovidQueue,
+    JobsTopBar,
+    MyMessage,
     SidebarLeft,
     SidebarRight,
     DonationAskModal,
-    AvailabilityModal
+    AvailabilityModal,
+    ExpectedRepliesWarning
   },
   mixins: [loginRequired, buildHead, waitForRef],
   data() {
@@ -51,30 +277,14 @@ export default {
   },
   computed: {
     wanteds() {
-      const ret = []
-
-      for (const message of this.messages) {
-        if (message.type === 'Wanted') {
-          ret.push(message)
-        }
-      }
-
+      const ret = this.messages.filter(m => m.type === 'Wanted' && !m.isdraft)
       ret.sort(this.postSort)
-
       return ret
     },
 
     offers() {
-      const ret = []
-
-      for (const message of this.messages) {
-        if (message.type === 'Offer') {
-          ret.push(message)
-        }
-      }
-
+      const ret = this.messages.filter(m => m.type === 'Offer' && !m.isdraft)
       ret.sort(this.postSort)
-
       return ret
     },
 
@@ -144,6 +354,17 @@ export default {
 
       return count
     },
+
+    queued() {
+      const ret = this.messages.filter(m => m.isdraft)
+      ret.sort(this.postSort)
+      return ret
+    },
+
+    queuedCount() {
+      return this.queued.length
+    },
+
     searches() {
       // Show the searches within the last 90 days, most recent first.  Anything older is less likely to be relevant
       // and it stops it growing forever, forcing them to delete things.
@@ -190,6 +411,14 @@ export default {
       const currentCount = this.messages.length
 
       try {
+        // COVID - load any queued posts.
+        await this.$store.dispatch('messages/fetchMessages', {
+          collection: 'Draft',
+          types: ['Offer'],
+          fromuser: me.id,
+          limit: 1000
+        })
+
         await this.$store.dispatch('messages/fetchMessages', {
           collection: 'AllUser',
           summary: true,
