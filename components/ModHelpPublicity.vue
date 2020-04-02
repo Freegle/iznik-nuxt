3bcd61b9d79fd399903a2ad3d4f7b43087db625a
Edward Hibbert <edward@ehibbert.org.uk>
2020-04-02 12:27:21 +0100

    MT: Improve publicity help

--- components/ModHelpPublicity.vue
+++ components/ModHelpPublicity.vue
@@ -5,11 +5,15 @@
     </b-btn>
     <p>
       These are actions you can take to promote your group. For example, you will see social media posts which
-      you might like to share to your groups' Facebook pages.
+      you might like to share to your communities' Facebook pages.
     </p>
     <p>
       You don't have to use Facebook yourself to do this. The share to Facebook will happen magically on the
       servers if you click the share buttons below.
+    </p>
+    <p>
+      If you don't want to share a particular item, share any others that you do, then click the <em>Hide All</em>
+      button.  This will hide it for all mods on this community.
     </p>
     <p>
       We can't do this automatically - as per
