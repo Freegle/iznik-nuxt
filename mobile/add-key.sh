994d874e9da1a21ea691d71e3fbfb1d8a3c36367
chriscant <chris@phdcc.com>
2020-04-04 10:54:48 +0100

    Try keychain fix

--- mobile/add-key.sh
+++ mobile/add-key.sh
@@ -18,9 +18,12 @@ security unlock-keychain -p travis ios-build.keychain
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
 # Add certificates to keychain and allow codesign to access them
-security import mobile/AppleWWDRCA.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import Freegle0320.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import Freegle0320.p12 -k ~/Library/Keychains/ios-build.keychain -P $iossignkey -T /usr/bin/codesign
+security import mobile/AppleWWDRCA.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign -T /usr/bin/productsign
+security import Freegle0320.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign -T /usr/bin/productsign
+security import Freegle0320.p12 -k ~/Library/Keychains/ios-build.keychain -P $iossignkey -T /usr/bin/codesign -T /usr/bin/productsign
+
+# https://stackoverflow.com/a/40870033/1972627
+security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k travis ios-build.keychain
 
 # Put the provisioning profile in place
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
