13fbeb4efc5f957156c30e3828401814ec53d36c
chriscant <chris@phdcc.com>
2020-04-04 09:46:46 +0100

    Try different cert

--- mobile/add-key.sh
+++ mobile/add-key.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -x #echo on
 
 # Create a custom keychain
 security create-keychain -p travis ios-build.keychain
@@ -18,8 +19,8 @@ security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
 # Add certificates to keychain and allow codesign to access them
 security import mobile/AppleWWDRCA.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import FreegleChris.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import FreegleChris.p12 -k ~/Library/Keychains/ios-build.keychain -P $iossignkey -T /usr/bin/codesign
+security import Freegle0320.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import Freegle0320.p12 -k ~/Library/Keychains/ios-build.keychain -P $iossignkey -T /usr/bin/codesign
 
 # Put the provisioning profile in place
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
