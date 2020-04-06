44b8798f45996aa1e7df55fe197ccc4d74e5212e
Jason Harris <jayyaj@hotmail.com>
2020-04-06 16:03:06 +0100

    Add an overall focus style for the password group

--- pages/settings/index.vue
+++ pages/settings/index.vue
@@ -192,9 +192,9 @@
                     label="Your password:"
                   >
                     <b-input-group id="input-password">
-                      <b-input v-model="me.password" :type="showPassword ? 'text' : 'password'" placeholder="Your password" label="Your password" />
+                      <b-input v-model="me.password" :type="showPassword ? 'text' : 'password'" placeholder="Your password" label="Your password" class="password__input" />
+                      <span class="password__focus-element" />
                       <b-input-group-append>
-                        <!-- TODO RAHUL DESIGN MINOR The shadow on the input field that you get when you're focused ought really to include this append.-->
                         <b-button variant="white" class="transbord" title="Show password" @click="togglePassword">
                           <v-icon v-if="showPassword" title="Hide password" class="text-secondary" flip="horizontal">
                             <v-icon name="eye" />
@@ -958,5 +958,25 @@ export default {
 
 h4 a {
   color: $colour-header;
+}
+
+/* These classes are to give the complete password group of elements a focus indicator */
+/* when the individual password input element gets focus.  It should not display for the */
+/* other two individual elements */
+.password__input:focus {
+  /* Get rid of any default focus on the password input element added by bootstrap */
+  box-shadow: none;
+  border-right-color: $color-black;
+}
+
+.password__input:focus + .password__focus-element {
+  /* Add bootstrap style focus to the span element.  This element should encompass */
+  /* the whole password group */
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  padding: 1px;
+  box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
+  border-radius: 3px;
 }
 </style>
