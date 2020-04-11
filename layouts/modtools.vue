3a84c9286d5894e5a8eec34fdc1456a4592135ab
Edward Hibbert <edward@ehibbert.org.uk>
2020-04-11 10:24:28 +0100

    MT: More on layout

--- layouts/modtools.vue
+++ layouts/modtools.vue
@@ -100,7 +100,7 @@
             </a>
           </div>
         </div>
-        <nuxt ref="pageContent" class="ml-0 pl-0 pl-sm-1 pr-0 pr-sm-1 pageContent" />
+        <nuxt ref="pageContent" class="ml-0 pl-0 pl-sm-1 pr-0 pr-sm-1 pageContent flex-grow-1" />
       </div>
       <ChatPopups v-if="loggedIn" class="d-none d-sm-block" />
       <LoginModal ref="loginModal" />
