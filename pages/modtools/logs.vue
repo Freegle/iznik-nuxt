bf761cb8a8e59ae135cf8149bcd5db21f183c284
Edward Hibbert <edward@ehibbert.org.uk>
2020-04-18 16:11:01 +0100

    MT: Search in member logs

--- pages/modtools/logs.vue
+++ pages/modtools/logs.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-white">
     <b-tabs content-class="mt-3" card>
-      <b-tab active @click="clear('messages')">
+      <b-tab active @click="clear('messages', true)">
         <template v-slot:title>
           <h2 class="ml-2 mr-2">
             Messages
@@ -28,13 +28,32 @@
           </b-input-group>
         </div>
       </b-tab>
-      <b-tab to="/modtools/logs/members" @click="clear('memberships')">
+      <b-tab to="/modtools/logs/members" @click="clear('memberships', true)">
         <template v-slot:title>
           <h2 class="ml-2 mr-2">
             Members
           </h2>
         </template>
-        <GroupSelect v-model="groupid" modonly />
+        <div class="d-flex flex-wrap">
+          <GroupSelect v-model="groupid" modonly />
+          <b-input-group class="flex max">
+            <b-form-input
+              v-model="term"
+              type="text"
+              placeholder="Search name/email/subject"
+              autocapitalize="none"
+              @keyup.native.enter="search"
+            />
+            <b-input-group-append>
+              <b-button
+                variant="success"
+                @click="search"
+              >
+                <v-icon name="search" />
+              </b-button>
+            </b-input-group-append>
+          </b-input-group>
+        </div>
       </b-tab>
     </b-tabs>
     <ModLogs v-if="groupid" :key="'modlogs-' + bump" class="bg-white" :groupid="groupid" />
@@ -66,9 +85,14 @@ export default {
     this.clear(this.type)
   },
   methods: {
-    clear(type) {
+    clear(type, term) {
       this.bump = Date.now()
       this.type = type
+
+      if (term) {
+        this.term = null
+      }
+
       this.$store.dispatch('logs/setParams', {
         type: type,
         search: this.term
