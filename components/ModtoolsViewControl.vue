044075908739f71387cc91c556c92bbe0a17af93
Edward Hibbert <edward@ehibbert.org.uk>
2020-04-27 13:18:02 +0100

    MT: Summary view use toggle

--- components/ModtoolsViewControl.vue
+++ components/ModtoolsViewControl.vue
@@ -1,15 +1,21 @@
 <template>
-  <div>
-    <b-btn v-if="summary" variant="white" @click="toggleView">
-      <v-icon name="th-list" /> Details View
-    </b-btn>
-    <b-btn v-else variant="white" @click="toggleView">
-      <v-icon name="th-list" /> Summary View
-    </b-btn>
-  </div>
+  <OurToggle
+    :value="summary"
+    class="mt-2"
+    :height="30"
+    :width="150"
+    :font-size="14"
+    :sync="true"
+    :labels="{checked: 'Summary View', unchecked: 'Detailed View'}"
+    color="#61AE24"
+    @change="toggleView"
+  />
 </template>
 <script>
+const OurToggle = () => import('~/components/OurToggle')
+
 export default {
+  components: { OurToggle },
   computed: {
     summary() {
       const ret = this.$store.getters['misc/get'](
@@ -19,10 +25,10 @@ export default {
     }
   },
   methods: {
-    toggleView() {
+    toggleView(c, e) {
       this.$store.dispatch('misc/set', {
         key: 'modtoolsMessagesApprovedSummary',
-        value: !this.summary
+        value: c.value
       })
     }
   }
