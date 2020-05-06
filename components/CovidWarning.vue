8a070673234ab885c331712769fc4b3a17173aeb
Edward Hibbert <edward@ehibbert.org.uk>
2020-05-06 15:07:22 +0100

    Add safe freegling advice

--- components/CovidWarning.vue
+++ components/CovidWarning.vue
@@ -1,45 +1,88 @@
 <template>
   <NoticeMessage v-if="show" variant="danger">
-    <b-btn variant="link" class="float-right" @click="hideit">
-      Hide this
+    <!--    <b-btn variant="link" class="float-right" @click="hideit">-->
+    <!--      Hide this-->
+    <!--    </b-btn>-->
+    <p>
+      We're back!  Tips and recycling centres are re-opening, and the <a rel="noopener noreferrer" target="_blank" href="https://www.gov.uk/government/publications/coronavirus-covid-19-advice-to-local-authorities-on-prioritising-waste-collections/managing-household-waste-and-recycling-centres-hwrcs-in-england-during-the-coronavirus-covid-19-pandemic">government advice</a> allows online platforms such as ours.
+    </p>
+    <p>
+      We hope you and yours are safe.  Please freegle responsibly and help keep it that way.
+    </p>
+    <b-btn variant="primary" @click="showModal = true">
+      Read our advice on safe freegling
     </b-btn>
-    <p>
-      <b>Very sadly, Freegle has suspended people collecting items due to COVID-19.</b>
-    </p>
-    <p>
-      But you can still offer your stuff and we'll let people know about it when we re-open.  That way you can declutter
-      while you're stuck at home.  Don't mope and mutter, just de-clutter!
-    </p>
-    <b-btn variant="success" size="lg" to="/give" class="mb-2">
-      <v-icon name="gift" />&nbsp;Give Stuff
-    </b-btn>
-    <p>
-      We’ll email you
-      when we’re back.  There will be
-      a lot of people who are struggling and will need things then.
-    </p>
-    <p>
-      Freegle also needs your help.  We are funded mostly from donations given by freeglers - no freegling means we
-      won’t be getting donations.  Lots of charities and businesses will be struggling, but if you want us to
-      be here for you in six months’ time then please set up a regular donation:
-
-      <DonationButton link="paypal1" show="£1/month" monthly class="mt-2" />
-    </p>
-    <div v-if="$route.path !== '/chitchat'">
+    <b-modal v-model="showModal" ok-only size="lg" title="Safe Freegling" ok-title="Close">
       <p>
-        Our <nuxt-link to="/chitchat">
-          ChitChat section
-        </nuxt-link> is staying open.  Join us here to swap hints,
-        tips and tell others how you’re filling your day.  Share your knowledge and questions on upcycling, reuse,
-        crafts, gardening, cooking, hobbies etc.
+        As tips and recycling centres start to re-open to varying degrees around the U.K we think it’s a good time
+        to reboot Freegle. We know freegling will be immensely useful right now.
       </p>
-      <b-btn to="/chitchat" variant="white" size="lg" class="mb-2">
-        Join us on ChitChat <v-icon name="angle-double-right" />
-      </b-btn>
-    </div>
-    <p>
-      See you on the other side!
-    </p>
+      <p>
+        We do not want to encourage unnecessary travel and we’d like everyone to stay safe so please
+        <b>READ</b> and <b>FOLLOW</b> these updated COVID-19 guidelines for freegling.
+      </p>
+      <h4>Keep it ESSENTIAL</h4>
+      <p>
+        Initially we’d like to encourage OFFERs and WANTEDs for <b>essential</b> items only.  We know that will vary
+        for different people and circumstances.  Please use your common sense &#x1F642;.  Many communities will have
+        restrictions on what is and isn’t OK to freegle, so please respect this.
+      </p>
+      <h4>Keep it LOCAL</h4>
+      <p>
+        Please don’t travel further than you would be doing so anyway (to buy food, essentials or to go to work).
+      </p>
+      <p>
+        Avoid special or extra journeys for freegling - see Government advice on
+        <a href="https://www.gov.uk/government/publications/full-guidance-on-staying-at-home-and-away-from-others/full-guidance-on-staying-at-home-and-away-from-others" target="_blank" rel="noopener noreferrer">when you can leave the house</a>.
+        Could you collect on foot or cycle while doing daily exercise?
+      </p>
+      <p>
+        Only post to your nearest community - don’t post or reply if it’s further away.
+      </p>
+      <h4>PROTECT each other</h4>
+      <p>
+        Make sure anything you offer is clean.
+      </p>
+      <ul>
+        <li>
+          Wash fabric things.
+        </li>
+        <li>
+          Wipe down hard surfaces with hot soapy water.
+        </li>
+      </ul>
+      <p>
+        Assume anything you collect <b>isn’t</b> clean (better safe than sorry).  Clean it yourself, and if possible
+        leave it for a few days before touching it.
+      </p>
+      <p>
+        Wear a face mask if you can when collecting, and wash your hands afterwards.
+      </p>
+      <h4>SOCIAL DISTANCING</h4>
+      <p>
+        Observe SOCIAL-DISTANCING and make CONTACT-LESS arrangements for collections.  Do not enter anyone else’s home
+        or let them enter yours.  This might mean holding off listing bulky items that need a few people to move!
+      </p>
+      <h4>COVID-vulnerable?</h4>
+      <p>
+        As much as we’d like everyone to benefit from freegling, if you are COVID-vulnerable please do not freegle
+        and exchange goods (though you’re welcome to ask for help with supplies via our
+        <nuxt-link to="/chitchat">
+          ChitChat
+        </nuxt-link> section
+      </p>
+      <h4>And finally...</h4>
+      <p>
+        As always be kind, polite and don’t waste any	one’s time!
+      </p>
+      <p>
+        Stay safe everyone!
+      </p>
+      <p>
+        P.S. If you're super-happy we're back, any small donations would be super-helpful right now - thank you!
+      </p>
+      <DonationButton link="paypal1510" class="mt-2" />
+    </b-modal>
   </NoticeMessage>
   <div v-else class="text-danger text-right clickme" @click="showit">
     Show COVID-19 warning.
@@ -48,8 +91,14 @@
 <script>
 import NoticeMessage from './NoticeMessage'
 import DonationButton from './DonationButton'
+
 export default {
   components: { DonationButton, NoticeMessage },
+  data: function() {
+    return {
+      showModal: false
+    }
+  },
   computed: {
     show() {
       return !this.$store.getters['misc/get']('hidecovidwarning')
