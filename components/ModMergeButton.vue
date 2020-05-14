506eef9492e7288cf6f4bc7082d0e934533778d9
Edward Hibbert <edward@ehibbert.org.uk>
2020-05-14 10:43:52 +0100

    MT: Show merge button in more places

---
+++ components/ModMergeButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <b-btn variant="white" @click="mergeMember">
+      <v-icon name="equals" /> Merge
+    </b-btn>
+    <ModMergeMemberModal ref="mergemodal" />
+  </div>
+</template>
+<script>
+import ModMergeMemberModal from './ModMergeMemberModal'
+import waitForRef from '@/mixins/waitForRef'
+
+export default {
+  components: { ModMergeMemberModal },
+  mixins: [waitForRef],
+  methods: {
+    mergeMember() {
+      this.waitForRef('mergemodal', () => {
+        this.$refs.mergemodal.show()
+      })
+    }
+  }
+}
+</script>
