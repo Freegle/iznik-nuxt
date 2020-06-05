82b6720b45103d24f661bc7c8c05fe06f0b3864a
Edward Hibbert <edward@ehibbert.org.uk>
2020-06-05 10:47:21 +0100

    MT: Don't show unbounce when you don't have permission to do it

--- components/ModBouncing.vue
+++ components/ModBouncing.vue
@@ -7,13 +7,18 @@
       <p>
         <a :href="user.email">{{ user.email }}</a> is bouncing - see logs for details.
       </p>
-      <p>If you think the email is valid, you can:</p>
-      <b-btn variant="white" @click="unbounce">
-        <v-icon v-if="unbouncing" name="sync" class="fa-spin text-success" />
-        <v-icon v-else-if="unbounced" name="check" class="text-success" />
-        <v-icon v-else name="sync" />
-        Reactivate
-      </b-btn>
+      <div v-if="user.role === 'Member'">
+        <p>If you think the email is valid, you can:</p>
+        <b-btn variant="white" @click="unbounce">
+          <v-icon v-if="unbouncing" name="sync" class="fa-spin text-success" />
+          <v-icon v-else-if="unbounced" name="check" class="text-success" />
+          <v-icon v-else name="sync" />
+          Reactivate
+        </b-btn>
+      </div>
+      <div v-else>
+        <p>You can't unbounce - you can only do this to members, and their role is {{ user.role }}.</p>
+      </div>
     </NoticeMessage>
   </div>
 </template>
