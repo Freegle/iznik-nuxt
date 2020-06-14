991394c9820feb09eb65bc3db9ccdbb908883d6c
Edward Hibbert <edward@ehibbert.org.uk>
2020-06-14 16:09:54 +0100

    MT: Improve styling of text differences

--- components/DiffPart.vue
+++ components/DiffPart.vue
@@ -15,7 +15,7 @@ export default {
       if (this.part.added) {
         return 'text-warning'
       } else if (this.part.removed) {
-        return 'strike text-danger'
+        return 'strike text-danger text-muted'
       } else {
         return 'text-black'
       }
@@ -32,8 +32,3 @@ export default {
   }
 }
 </script>
-<style scoped>
-::v-deep .strike {
-  text-decoration: line-through;
-}
-</style>
