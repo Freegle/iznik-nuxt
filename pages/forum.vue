741aff8a4ca9ea96b8d3f2376fb42b20510e0be9
Edward Hibbert <edward@ehibbert.org.uk>
2020-06-21 10:44:45 +0100

    Add forum redirect page

---
+++ pages/forum.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>This should redirect you back to our discussion forum..  If it doesn't, mail geeks@ilovefreegle.org.</p>
+    <b-img src="~/static/loader.gif" alt="Loading" />
+  </div>
+</template>
+<script>
+import loginRequired from '@/mixins/loginRequired.js'
+
+export default {
+  mixins: [loginRequired],
+  watch: {
+    myid(newVal, oldVal) {
+      if (!oldVal && newVal) {
+        this.redirect()
+      }
+    }
+  },
+  mounted() {
+    if (this.myid) {
+      this.redirect()
+    }
+  },
+  methods: {
+    redirect() {
+      window.location = 'https://forum.ilovefreegle.org'
+    }
+  }
+}
+</script>
