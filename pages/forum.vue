eb61a229bcbd8e56122e042c18c8e5dacf33bc29
Edward Hibbert <edward@ehibbert.org.uk>
2020-06-23 08:16:54 +0100

    Tidy a couple of pages a little

--- pages/forum.vue
+++ pages/forum.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <p>This should redirect you back to our discussion forum..  If it doesn't, mail geeks@ilovefreegle.org.</p>
+  <div class="w-100 bg-white text-center">
+    <p class="pt-4">
+      This should redirect you back to our discussion forum..  If it doesn't, mail geeks@ilovefreegle.org.
+    </p>
     <b-img src="~/static/loader.gif" alt="Loading" />
   </div>
 </template>
