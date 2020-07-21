4c83d09fe0efff4d03265895caaeb10afe5bb734
chriscant <chris@phdcc.com>
2020-07-21 09:07:22 +0100

    Sort marker for maps in app

--- plugins/vue2-leaflet.js
+++ plugins/vue2-leaflet.js
@@ -15,6 +15,16 @@ import {
 } from 'vue2-leaflet'
 import 'leaflet/dist/leaflet.css'
 
+import { Icon } from 'leaflet'
+if (process.env.IS_APP) { // CC
+  delete Icon.Default.prototype._getIconUrl
+  Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+  })
+}
+
 const VueLeaflet = {
   install(Vue, options) {
     Vue.component('l-map', LMap)
