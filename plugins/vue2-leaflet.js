9e023753cd1e137e7f1431f4bc5e4827bb1af582
Edward Hibbert <edward@ehibbert.org.uk>
2020-08-05 20:59:46 +0100

    Default map marker icon not showing on production builds.

--- plugins/vue2-leaflet.js
+++ plugins/vue2-leaflet.js
@@ -13,7 +13,17 @@ import {
   LFeatureGroup,
   LTooltip
 } from 'vue2-leaflet'
+import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
+
+// eslint-disable-next-line
+delete L.Icon.Default.prototype._getIconUrl
+// eslint-disable-next-line
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+})
 
 const VueLeaflet = {
   install(Vue, options) {
