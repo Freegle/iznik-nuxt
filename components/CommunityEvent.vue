17b093156b30c4fef6a217f5ec097820e5525064
Jason Harris <jayyaj@hotmail.com>
2020-08-05 14:37:52 +0100

    Fix the mobile shimmy on events

--- components/CommunityEvent.vue
+++ components/CommunityEvent.vue
@@ -51,58 +51,54 @@
             Posted on {{ item.groups[0].namedisplay }}
           </div>
         </div>
-        <div v-else>
-          <b-row>
-            <b-col cols="12" :md="item.photo ? 6 : 12">
-              <div v-if="item.earliestDate" class="media">
-                <div class="media-left">
-                  <div class="media-object pl-1 text-muted">
-                    <v-icon name="clock" class="fa-fw" />
-                  </div>
-                </div>
-                <div class="media-body ml-2">
-                  {{ item.earliestDate.string.start }} - {{ item.earliestDate.string.end }}
+        <div v-else class="event">
+          <div class="event__body">
+            <div v-if="item.earliestDate" class="media">
+              <div class="media-left">
+                <div class="media-object pl-1 text-muted">
+                  <v-icon name="clock" class="fa-fw" />
                 </div>
               </div>
-              <div class="media">
-                <div class="media-left">
-                  <div class="media-object pl-1 text-muted">
-                    <v-icon name="map-marker-alt" class="fa-fw" />
-                  </div>
-                </div>
-                <div class="media-body ml-2 small">
-                  {{ item.location }}
+              <div class="media-body ml-2">
+                {{ item.earliestDate.string.start }} - {{ item.earliestDate.string.end }}
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <div class="media-object pl-1 text-muted">
+                  <v-icon name="map-marker-alt" class="fa-fw" />
                 </div>
               </div>
-              <div class="media">
-                <div class="media-left">
-                  <div class="media-object pl-1 text-muted">
-                    <v-icon name="users" class="fa-fw" />
-                  </div>
-                </div>
-                <div v-if="item.groups && item.groups.length > 0" class="media-body ml-2 small">
-                  Posted on {{ item.groups[0].namedisplay }}
+              <div class="media-body ml-2 small">
+                {{ item.location }}
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <div class="media-object pl-1 text-muted">
+                  <v-icon name="users" class="fa-fw" />
                 </div>
               </div>
-              <read-more v-if="description" :text="description" :max-chars="300" class="ml-1 font-weight-bold preline forcebreak nopara" />
-              <div class="mt-2 mb-2 ml-1">
-                <b-btn variant="white" :aria-label="'More info about ' + item.title + ' event'" @click="showEventModal">
-                  <v-icon name="info-circle" /> More info
-                </b-btn>
+              <div v-if="item.groups && item.groups.length > 0" class="media-body ml-2 small">
+                Posted on {{ item.groups[0].namedisplay }}
               </div>
-            </b-col>
-            <b-col>
-              <b-img-lazy
-                v-if="item.photo"
-                :src="item.photo.path"
-                rounded
-                thumbnail
-                class="square float-right"
-                generator-unable-to-provide-required-alt=""
-                title="Event photo"
-              />
-            </b-col>
-          </b-row>
+            </div>
+            <read-more v-if="description" :text="description" :max-chars="300" class="ml-1 font-weight-bold preline forcebreak nopara" />
+            <div class="mt-2 mb-2 ml-1">
+              <b-btn variant="white" :aria-label="'More info about ' + item.title + ' event'" @click="showEventModal">
+                <v-icon name="info-circle" /> More info
+              </b-btn>
+            </div>
+          </div>
+          <b-img-lazy
+            v-if="item.photo"
+            :src="item.photo.path"
+            rounded
+            thumbnail
+            class="square float-right"
+            generator-unable-to-provide-required-alt=""
+            title="Event photo"
+          />
         </div>
       </b-card-body>
     </b-card>
@@ -154,6 +150,9 @@ export default {
 
 <style scoped lang="scss">
 @import 'color-vars';
+@import '~bootstrap/scss/functions';
+@import '~bootstrap/scss/variables';
+@import '~bootstrap/scss/mixins/_breakpoints';
 
 .square {
   object-fit: cover;
@@ -163,5 +162,18 @@ export default {
 
 .event__link {
   color: $color-blue--2;
+}
+
+.event {
+  display: flex;
+  flex-direction: column;
+
+  @include media-breakpoint-up(sm) {
+    flex-direction: row;
+  }
+}
+
+.event__body {
+  flex-grow: 1;
 }
 </style>
