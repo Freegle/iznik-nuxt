354a3c8ed136934219ed75dd15d735907227d2c6
Edward Hibbert <edward@ehibbert.org.uk>
2020-08-07 08:23:52 +0100

    Move message photos modal into separate component, in a way that seems to fix a Safari 10 problem that I don't understand.

---
+++ components/MessagePhotosModal.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <b-modal
+      :id="'photoModal-' + message.id"
+      v-model="showModal"
+      :title="subject"
+      size="lg"
+      no-stacking
+      ok-only
+    >
+      <template slot="default">
+        <ImageCarousel message-id="message.id" :attachments="message.attachments" />
+      </template>
+      <template slot="modal-footer" slot-scope="{ ok, cancel }">
+        <b-button variant="white" @click="cancel">
+          Close
+        </b-button>
+      </template>
+    </b-modal>
+  </div>
+</template>
+<script>
+const ImageCarousel = () => import('./ImageCarousel')
+
+export default {
+  components: { ImageCarousel },
+  props: {
+    message: {
+      type: Object,
+      required: true
+    },
+    subject: {
+      type: String,
+      required: true
+    }
+  },
+  data: function() {
+    return {
+      showModal: false
+    }
+  },
+  methods: {
+    show() {
+      this.showModal = true
+    },
+    hide() {
+      this.showModal = false
+    }
+  }
+}
+</script>
