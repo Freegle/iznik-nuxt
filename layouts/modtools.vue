aeaaed58b6ca5c5f07f716e540bfd7e19178afe9
Edward Hibbert <edward@ehibbert.org.uk>
2020-08-08 09:37:40 +0100

    MT: Try again to get favicon right

--- layouts/modtools.vue
+++ layouts/modtools.vue
@@ -205,6 +205,8 @@ export default {
     this.$store.dispatch('modconfigs/fetch', {
       all: true
     })
+
+    setTimeout(this.updateFavicon, 1000)
   },
   beforeDestroy() {
     if (this.workTimer) {
@@ -212,6 +214,24 @@ export default {
     }
   },
   methods: {
+    updateFavicon() {
+      if (process.client) {
+        // This is a bit of a hack, but seems necessary to make the favicon stick.
+        //
+        // Check if it's ok first, as otherwise we keep fetching the icon.
+        let link = document.querySelector("link[rel*='icon']")
+
+        if (!link) {
+          link = document.createElement('link')
+          link.type = 'image/x-icon'
+          link.rel = 'icon'
+          link.href = require('~/static/icon_modtools.png')
+          document.getElementsByTagName('head')[0].appendChild(link)
+        } else if (link.href.indexOf('icon_modtools.png') === -1) {
+          link.href = require('~/static/icon_modtools.png')
+        }
+      }
+    },
     async logOut() {
       // Remove all cookies, both client and server.  This seems to be necessary to kill off the PHPSESSID cookie
       // on the server, which would otherwise keep us logged in despite our efforts.
