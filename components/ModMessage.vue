d3972fea42d92ba81273d7d0f82f13b39edeaba8
Edward Hibbert <edward@ehibbert.org.uk>
2020-08-21 17:03:23 +0100

    MT: improve edit layout on mobile

--- components/ModMessage.vue
+++ components/ModMessage.vue
@@ -4,11 +4,13 @@
       <b-card-header class="p-1 p-md-2">
         <div class="d-flex justify-content-between">
           <div class="flex-grow-1">
-            <div v-if="editing" class="d-flex">
+            <div v-if="editing" class="d-flex flex-wrap">
               <GroupSelect v-model="editgroup" modonly class="mt-1 mr-1" />
               <div v-if="message.item && message.location" class="d-flex justify-content-start">
                 <b-select v-model="message.type" :options="typeOptions" class="type mr-1" size="lg" />
                 <b-input v-model="message.item.name" size="lg" class="mr-1" />
+              </div>
+              <div v-if="message.item && message.location">
                 <b-input-group>
                   <Postcode :value="message.location.name" :find="false" @selected="postcodeSelect" />
                 </b-input-group>
