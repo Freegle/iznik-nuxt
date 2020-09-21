b0e6d235f4851b096ce66e55f02572894098810c
Jason Harris <jayyaj@hotmail.com>
2020-09-21 10:16:29 +0100

    Add a speech bubble to the message text

--- components/VisualiseSpeech.vue
+++ components/VisualiseSpeech.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="thick wide bg-white text-center rounded pt-1">
+  <div class="thick wide bg-white text-center rounded pt-1 speech-bubble">
     <h3>
       {{ text }}
     </h3>
@@ -29,5 +29,31 @@ export default {
   width: 200px;
   left: -80px;
   top: -55px;
+}
+
+.speech-bubble:before {
+  content: '';
+  width: 0px;
+  height: 0px;
+  position: absolute;
+  border-left: 10px solid $color-green--darker;
+  border-right: 10px solid transparent;
+  border-top: 10px solid $color-green--darker;
+  border-bottom: 10px solid transparent;
+  left: 130px;
+  bottom: -20px;
+}
+
+.speech-bubble:after {
+  content: '';
+  width: 0px;
+  height: 0px;
+  position: absolute;
+  border-left: 10px solid $color-white;
+  border-right: 10px solid transparent;
+  border-top: 10px solid $color-white;
+  border-bottom: 10px solid transparent;
+  left: 133px;
+  bottom: -13px;
 }
 </style>
