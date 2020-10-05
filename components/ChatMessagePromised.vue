b000fc059d06b924744c7cd74843c626ea6c5122
Edward Hibbert <edward@ehibbert.org.uk>
2020-10-05 21:53:30 +0100

    Unpromise button not showing in chat

--- components/ChatMessagePromised.vue
+++ components/ChatMessagePromised.vue
@@ -54,7 +54,7 @@
               <h4>
                 {{ refmsg.subject }}
               </h4>
-              <b-btn v-if="refmsg.promised && !hasOutcome" variant="warning" class="align-middle mt-1 mb-1" @click="unpromise">
+              <b-btn v-if="refmsg.promisecount && !hasOutcome" variant="warning" class="align-middle mt-1 mb-1" @click="unpromise">
                 <v-icon>
                   <v-icon name="handshake" />
                   <v-icon
