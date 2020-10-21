8d27de48e9c34e50243756826b9c39d56462b7cd
Edward Hibbert <edward@ehibbert.org.uk>
2020-10-21 13:15:10 +0100

    Show Wales in MT posting maps for firebreak

--- components/ModMessage.vue
+++ components/ModMessage.vue
@@ -119,7 +119,7 @@
             </div>
           </b-col>
           <b-col cols="12" lg="3">
-            <MessageMap v-if="group && position" :centerat="{ lat: group.lat, lng: group.lng }" :position="{ lat: position.lat, lng: position.lng }" />
+            <MessageMap v-if="group && position" :centerat="{ lat: group.lat, lng: group.lng }" :position="{ lat: position.lat, lng: position.lng }" wales />
           </b-col>
           <b-col cols="12" lg="3">
             <div class="rounded border border-info p-2 d-flex justify-content-between flex-wrap">
