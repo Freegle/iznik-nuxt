cc07f6bcba0e9e550601fb37744dd14a385a7079
Edward Hibbert <edward@ehibbert.org.uk>
2020-11-10 15:59:49 +0000

    Tweaks to quantity

--- components/NumberIncrementDecrement.vue
+++ components/NumberIncrementDecrement.vue
@@ -1,34 +1,23 @@
 <template>
-  <div class="d-flex flex-column text-center border rounded width position-relative pt-2">
-    <label id="incdec-label" class="text-muted">{{ label }}</label>
-    <b-input-group class="p-1">
-      <b-input-group-prepend class="d-flex flex-column justify-content-center">
-        <b-btn variant="white" class="attendance-button d-grid align-content-center justify-content-center leftbutt" tabindex="-1" @click="dec">
-          <v-icon name="minus" title="Increment" />
-        </b-btn>
-      </b-input-group-prepend>
-      <b-input
-        v-model="current"
-        role="spinbutton"
-        class="text-center count"
-        type="number"
-        :min="min"
-        :aria-valuemin="min"
-        :max="max"
-        :aria-valuemax="max"
-        :aria-valuenow="current"
-        step="1"
-        aria-labelledby="incdec-label"
-      />
-      <b-input-group-append class="d-flex flex-column justify-content-center">
-        <b-btn variant="white" class="attendance-button d-grid align-content-center justify-content-center" tabindex="-1" @click="inc">
-          <v-icon name="plus" title="Decrement" />
-        </b-btn>
-      </b-input-group-append>
-    </b-input-group>
+  <div class="d-flex flex-column text-center width position-relative">
+    <label :for="'spinbutton-' + id" class="text-muted">{{ label }}</label>
+    <b-spinbutton
+      :id="'spinbutton-' + id"
+      v-model="current"
+      type="number"
+      :min="min"
+      :max="max"
+      step="1"
+      size="lg"
+    />
   </div>
 </template>
 <script>
+import Vue from 'vue'
+
+import { FormSpinbuttonPlugin } from 'bootstrap-vue'
+Vue.use(FormSpinbuttonPlugin)
+
 export default {
   props: {
     count: {
@@ -58,7 +47,8 @@ export default {
   },
   data() {
     return {
-      current: null
+      current: null,
+      id: Date.now()
     }
   },
   watch: {
@@ -72,20 +62,6 @@ export default {
   },
   mounted() {
     this.current = this.count
-  },
-  methods: {
-    inc() {
-      if (this.current < this.max) {
-        this.current++
-        this.$emit('update:count', this.current)
-      }
-    },
-    dec() {
-      if (this.current > this.min) {
-        this.current--
-        this.$emit('update:count', this.current)
-      }
-    }
   }
 }
 </script>
@@ -94,30 +70,13 @@ export default {
 @import '~bootstrap/scss/variables';
 @import '~bootstrap/scss/mixins/_breakpoints';
 
-.count {
-  font-weight: bold;
-}
-
-.count::-webkit-outer-spin-button,
-.count::-webkit-inner-spin-button {
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
-  margin: 0;
-}
-
-.leftbutt {
-  border-top-left-radius: 4px;
-  border-bottom-left-radius: 4px;
-}
-
 .width {
   width: 170px;
 }
 
 label {
   position: absolute;
-  top: -18px;
+  top: -27px;
   left: 40px;
   background-color: white;
   z-index: 1000;
--- components/PostMessage.vue
+++ components/PostMessage.vue
@@ -39,10 +39,11 @@
         </b-btn>
       </b-card>
     </div>
+    {{ availablenow }}
     <div class="subject-layout mb-1">
       <b-input v-model="type" disabled class="type text-uppercase bg-white mt-1" size="lg" />
       <PostItem ref="item" v-model="item" class="item pt-1" @input="itemType" />
-      <NumberIncrementDecrement v-if="type === 'Offer'" :count.sync="availablenow" label="Quantity" class="count" />
+      <NumberIncrementDecrement v-if="type === 'Offer'" :count.sync="availablenow" label="Quantity" class="count pt-1" />
     </div>
     <div>
       <NoticeMessage v-if="vague" variant="warning" class="mt-1 mb-1">
