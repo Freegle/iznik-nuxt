179d59032372f3c3715ab97082c354cab6bf576d
Edward Hibbert <edward@ehibbert.org.uk>
2020-11-15 09:05:21 +0000

    Move notice about when reply has been sent so that it's visible on mobile.

--- components/Message.vue
+++ components/Message.vue
@@ -119,12 +119,6 @@
         <div v-else>
           <CovidCheckList v-if="!confirmed" class="mt-2" @confirmed="confirmed = true" />
           <div v-if="confirmed">
-            <NoticeMessage v-if="sent" variant="info" class="d-block d-sm-none mb-1">
-              We've sent your message.  You can see replies in the
-              <nuxt-link to="/chats">
-                <v-icon name="comments" /> Chats
-              </nuxt-link> section.
-            </NoticeMessage>
             <EmailValidator
               v-if="!me"
               ref="email"
@@ -156,6 +150,12 @@
                 class="flex-grow-1"
               />
             </b-form-group>
+            <NoticeMessage v-if="sent" variant="info" class="d-block d-sm-none mb-1">
+              We've sent your message.  You can see replies in the
+              <nuxt-link to="/chats">
+                <v-icon name="comments" /> Chats
+              </nuxt-link> section.
+            </NoticeMessage>
             <div v-if="!me">
               <div class="contents">
                 <div>
