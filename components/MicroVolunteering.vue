0e45477f6c7947f37d9448f0c2f5549d64f03838
edwh <edward@ehibbert.org.uk>
2020-11-27 21:21:31 +0000

    Microvolunteering "Don't ask again" should close popup

--- components/MicroVolunteering.vue
+++ components/MicroVolunteering.vue
@@ -326,7 +326,7 @@ export default {
               variant: 'inviterejected'
             })
 
-            this.$store.dispatch('user/edit', {
+            await this.$store.dispatch('user/edit', {
               id: this.myid,
               trustlevel: 'Declined'
             })
