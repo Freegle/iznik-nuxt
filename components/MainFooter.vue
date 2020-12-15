174866893e1e1e84f71c17c2793e23b778241365
Jason Harris <jayyaj@hotmail.com>
2020-12-15 08:45:18 +0000

    Improve the spacing for the footer

--- components/MainFooter.vue
+++ components/MainFooter.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="footer w-100">
+  <div class="footer w-100 mt-4">
     <div class="d-flex flex-column">
-      <div class="d-flex justify-content-center mt-4">
+      <div class="d-flex justify-content-center mb-2">
         <a target="_blank" rel="noopener" href="https://www.bytemark.co.uk/company/case-studies/freegle/" class="d-flex justify-content-start flex-column mr-4">
           <img src="/bytemark.png" alt="Bytemark Logo" class="sponsor">
         </a>
@@ -9,7 +9,7 @@
           <img src="/mythic-beasts.png" alt="Mythic Beasts Logo" class="sponsor">
         </a>
       </div>
-      <div class="d-flex flex-column justify-content-end pl-2 pr-2">
+      <div class="d-flex flex-column justify-content-end pl-2 pr-2 mb-2">
         <div class="text-muted text-center mb-1">
           Freegle is registered as a charity with HMRC (ref. XT32865). Kindly supported by Bytemark and Mythic Beasts.
         </div>
