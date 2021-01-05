3122d1c77cb1abddacf0b1a43ccc9850d10c7022
Jason Harris <jayyaj@hotmail.com>
2021-01-05 15:46:47 +0000

    Add a few comments to the code

--- components/Observer.vue
+++ components/Observer.vue
@@ -15,12 +15,14 @@ export default {
   }),
   mounted() {
     const options = this.options || {}
+    // Use array destructing on the entry argument i.e. entries[0]
     this.observer = new IntersectionObserver(([entry]) => {
       if (entry && entry.isIntersecting) {
         this.$emit('intersect')
       }
     }, options)
 
+    // this.$el is the root element of the component
     this.observer.observe(this.$el)
   },
   destroyed() {
