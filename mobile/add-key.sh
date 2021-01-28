fbf301c43c62f989db196451bd12f5e1dc4c5e5c
chriscant <chris@phdcc.com>
2021-01-28 10:05:28 +0000

    Tweak circleci

--- mobile/add-key.sh
+++ mobile/add-key.sh
@@ -2,13 +2,13 @@
 set -x #echo on
 
 # Create a custom keychain
-security create-keychain -p travis ios-build.keychain
+security create-keychain -p circleci ios-build.keychain
 
 # Make the custom keychain default, so xcodebuild will use it for signing
 security default-keychain -s ios-build.keychain
 
 # Unlock the keychain
-security unlock-keychain -p travis ios-build.keychain
+security unlock-keychain -p circleci ios-build.keychain
 
 # Added: unlock default keychain https://stackoverflow.com/questions/46295291/allowprovisioningupdates-doesnt-work/47076629#47076629
 # security unlock-keychain ~/Library/Keychains/login.keychain-db
@@ -19,11 +19,11 @@ security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
 # Add certificates to keychain and allow codesign to access them
 security import mobile/AppleWWDRCA.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign -T /usr/bin/xcrun
-security import Freegle0320.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign -T /usr/bin/xcrun
-security import Freegle0320.p12 -k ~/Library/Keychains/ios-build.keychain -P $iossignkey -T /usr/bin/codesign -T /usr/bin/xcrun
+security import mobile/secrets/Freegle0320.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign -T /usr/bin/xcrun
+security import mobile/secrets/Freegle0320.p12 -k ~/Library/Keychains/ios-build.keychain -P $iossignkey -T /usr/bin/codesign -T /usr/bin/xcrun
 
 # https://stackoverflow.com/a/40870033/1972627
-security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k travis ios-build.keychain
+security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k circleci ios-build.keychain
 
 # Put the provisioning profile in place
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
