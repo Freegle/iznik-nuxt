1e21801c8138792b0031007c7598bf1fce69cbcb
Edward Hibbert <edward@ehibbert.org.uk>
2021-02-03 07:33:07 +0000

    Move position of video ad in DOM

--- components/PixFuture.vue
+++ components/PixFuture.vue
@@ -5,7 +5,6 @@
 export default {
   mounted() {
     if (process.client) {
-      console.log('Add PixFuture')
       window.corner_video = 'left'
       window.zone_id = 131
       window.mobile_allowed = true
@@ -13,8 +12,7 @@ export default {
 
       const s = document.createElement('script')
       s.setAttribute('src', 'https://serv-vdo.pixfuture.com/vpaid/ads.js')
-      document.head.appendChild(s)
-      console.log('Added PixFuture')
+      document.body.appendChild(s)
     }
   }
 }
