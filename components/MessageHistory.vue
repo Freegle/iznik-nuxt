5e18f49a9223a10d741ab2e829e412431b551e72
Edward Hibbert <edward@ehibbert.org.uk>
2021-02-24 08:20:20 +0000

    MT: Another nit for Wanda

--- components/MessageHistory.vue
+++ components/MessageHistory.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div v-for="group in message.groups" :key="'message-' + message.id + '-' + group.id" class="text--small">
-      <span class="time">{{ group.arrival | timeago }} on</span> <nuxt-link :to="'/explore/' + exploreLink(group)">
+      <span class="time" :title="group.arrival">{{ group.arrival | timeago }} on</span> <nuxt-link :to="'/explore/' + exploreLink(group)">
         {{ group.namedisplay }}
       </nuxt-link>
       <client-only>
