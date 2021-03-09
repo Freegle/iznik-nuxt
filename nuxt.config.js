c1306eb57ecad913abcb8917dbb0370edc5778be
Edward Hibbert <edward@ehibbert.org.uk>
2021-03-09 09:16:42 +0000

    Possible fix for Sentry color scheme error

--- nuxt.config.js
+++ nuxt.config.js
@@ -74,6 +74,7 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'author', name: 'author', content: 'Freegle' },
       { name: 'supported-color-schemes', content: 'light' },
+      { name: 'color-scheme', content: 'light' },
       { hid: 'og:type', property: 'og:type', content: 'website' },
       {
         hid: 'description',
