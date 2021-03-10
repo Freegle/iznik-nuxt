c715043f5f02d7cf8cf46fe6c609eee2aeca1ae9
Edward Hibbert <edward@ehibbert.org.uk>
2021-03-10 08:11:15 +0000

    Allow carousel animation again

--- components/ImageCarousel.vue
+++ components/ImageCarousel.vue
@@ -7,7 +7,6 @@
       controls
       indicators
       img-width="100%"
-      no-animation
     >
       <b-carousel-slide v-for="(attachment,index) in attachments" :key="'mesagephohoto-' + attachment.id">
         <b-img
