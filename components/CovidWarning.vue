6982ab7989568daf6de2f70f47d7ff57fa7c08d3
Edward Hibbert <edward@ehibbert.org.uk>
2021-04-15 11:28:58 +0100

    Add nudge onsite to vote for PayPal competition

--- components/CovidWarning.vue
+++ components/CovidWarning.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <p v-if="!voted" class="text-center bg-white pulsate" @click="vote">
+      <nuxt-link to="/voteforfreegle" class="text-danger font-weight-bold">
+        Please vote for Freegle to win much-needed funds from PayPal!  Click here to find out more.
+      </nuxt-link>
+    </p>
     <NoticeMessage v-if="show" variant="danger">
       <b-btn variant="link" class="float-right" @click="hideit">
         Hide this
@@ -34,6 +39,9 @@ export default {
   computed: {
     show() {
       return !this.$store.getters['misc/get']('hidecovidwarning')
+    },
+    voted() {
+      return this.$store.getters['misc/get']('paypalvoted')
     }
   },
   methods: {
@@ -47,6 +55,12 @@ export default {
       this.$store.dispatch('misc/set', {
         key: 'hidecovidwarning',
         value: false
+      })
+    },
+    vote() {
+      this.$store.dispatch('misc/set', {
+        key: 'paypalvoted',
+        value: true
       })
     }
   }
