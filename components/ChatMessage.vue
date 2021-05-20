4823f6bd96f77b854f79cf08dea2e3b823a21891
Edward Hibbert <edward@ehibbert.org.uk>
2021-05-20 18:12:00 +0100

    Remove brief "unknow chat message type" error

--- components/ChatMessage.vue
+++ components/ChatMessage.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    <chat-message-text
-      v-if="chatmessage.type === 'Default'"
-      :chat="chat"
-      :chatmessage="chatmessage"
-      :otheruser="otheruser"
-      :pov="pov"
-      :chatusers="chatusers"
-      :highlight-emails="highlightEmails"
-    />
+    <div v-if="chatmessage.type === 'Default'">
+      <chat-message-text
+        :chat="chat"
+        :chatmessage="chatmessage"
+        :otheruser="otheruser"
+        :pov="pov"
+        :chatusers="chatusers"
+        :highlight-emails="highlightEmails"
+      />
+    </div>
     <chat-message-image
       v-else-if="chatmessage.type === 'Image'"
       :chat="chat"
@@ -17,87 +18,105 @@
       :pov="pov"
       :chatusers="chatusers"
     />
-    <chat-message-interested
-      v-else-if="chatmessage.type === 'Interested' && otheruser"
-      :chat="chat"
-      :chatmessage="chatmessage"
-      :otheruser="otheruser"
-      :pov="pov"
-      :chatusers="chatusers"
-      :highlight-emails="highlightEmails"
-    />
-    <chat-message-completed
-      v-else-if="chatmessage.type === 'Completed' && otheruser"
-      :chat="chat"
-      :chatmessage="chatmessage"
-      :otheruser="otheruser"
-      :pov="pov"
-      :chatusers="chatusers"
-    />
-    <chat-message-promised
-      v-else-if="chatmessage.type === 'Promised' && otheruser"
-      :chat="chat"
-      :chatmessage="chatmessage"
-      :otheruser="otheruser"
-      :pov="pov"
-      :chatusers="chatusers"
-    />
-    <chat-message-reneged
-      v-else-if="chatmessage.type === 'Reneged' && otheruser"
-      :chat="chat"
-      :chatmessage="chatmessage"
-      :otheruser="otheruser"
-      :pov="pov"
-      :chatusers="chatusers"
-    />
-    <chat-message-address
-      v-else-if="chatmessage.type === 'Address' && otheruser"
-      :chat="chat"
-      :chatmessage="chatmessage"
-      :otheruser="otheruser"
-      :pov="pov"
-      :chatusers="chatusers"
-    />
-    <chat-message-nudge
-      v-else-if="chatmessage.type === 'Nudge' && otheruser"
-      :chat="chat"
-      :chatmessage="chatmessage"
-      :otheruser="otheruser"
-      :pov="pov"
-      :chatusers="chatusers"
-    />
-    <chat-message-mod-mail
-      v-else-if="chatmessage.type === 'ModMail'"
-      :chat="chat"
-      :chatmessage="chatmessage"
-      :otheruser="null"
-      :pov="pov"
-      :chatusers="chatusers"
-    />
-    <chat-message-schedule
-      v-else-if="chatmessage.type === 'Schedule' && otheruser"
-      :chat="chat"
-      :chatmessage="chatmessage"
-      :otheruser="otheruser"
-      :pov="pov"
-      :chatusers="chatusers"
-    />
-    <chat-message-schedule
-      v-else-if="chatmessage.type === 'ScheduleUpdated' && otheruser"
-      :chat="chat"
-      :chatmessage="chatmessage"
-      :otheruser="otheruser"
-      :pov="pov"
-      :chatusers="chatusers"
-    />
-    <chat-message-report
-      v-else-if="chatmessage.type === 'ReportedUser'"
-      :chat="chat"
-      :chatmessage="chatmessage"
-      :otheruser="otheruser"
-      :pov="pov"
-      :chatusers="chatusers"
-    />
+    <div v-else-if="chatmessage.type === 'Interested'">
+      <chat-message-interested
+        v-if="otheruser"
+        :chat="chat"
+        :chatmessage="chatmessage"
+        :otheruser="otheruser"
+        :pov="pov"
+        :chatusers="chatusers"
+        :highlight-emails="highlightEmails"
+      />
+    </div>
+    <div v-else-if="chatmessage.type === 'Completed' && otheruser">
+      <chat-message-completed
+        v-if="otheruser"
+        :chat="chat"
+        :chatmessage="chatmessage"
+        :otheruser="otheruser"
+        :pov="pov"
+        :chatusers="chatusers"
+      />
+    </div>
+    <div v-else-if="chatmessage.type === 'Promised' && otheruser">
+      <chat-message-promised
+        v-if="otheruser"
+        :chat="chat"
+        :chatmessage="chatmessage"
+        :otheruser="otheruser"
+        :pov="pov"
+        :chatusers="chatusers"
+      />
+    </div>
+    <div v-else-if="chatmessage.type === 'Reneged' && otheruser">
+      <chat-message-reneged
+        v-if="otheruser"
+        :chat="chat"
+        :chatmessage="chatmessage"
+        :otheruser="otheruser"
+        :pov="pov"
+        :chatusers="chatusers"
+      />
+    </div>
+    <div v-else-if="chatmessage.type === 'Address' && otheruser">
+      <chat-message-address
+        v-if="otheruser"
+        :chat="chat"
+        :chatmessage="chatmessage"
+        :otheruser="otheruser"
+        :pov="pov"
+        :chatusers="chatusers"
+      />
+    </div>
+    <div v-else-if="chatmessage.type === 'Nudge' && otheruser">
+      <chat-message-nudge
+        v-if="otheruser"
+        :chat="chat"
+        :chatmessage="chatmessage"
+        :otheruser="otheruser"
+        :pov="pov"
+        :chatusers="chatusers"
+      />
+    </div>
+    <div v-else-if="chatmessage.type === 'ModMail'">
+      <chat-message-mod-mail
+        :chat="chat"
+        :chatmessage="chatmessage"
+        :otheruser="null"
+        :pov="pov"
+        :chatusers="chatusers"
+      />
+    </div>
+    <div v-else-if="chatmessage.type === 'Schedule' && otheruser">
+      <chat-message-schedule
+        v-if="otheruser"
+        :chat="chat"
+        :chatmessage="chatmessage"
+        :otheruser="otheruser"
+        :pov="pov"
+        :chatusers="chatusers"
+      />
+    </div>
+    <div v-else-if="chatmessage.type === 'ScheduleUpdated' && otheruser">
+      <chat-message-schedule
+        v-if="otheruser"
+        :chat="chat"
+        :chatmessage="chatmessage"
+        :otheruser="otheruser"
+        :pov="pov"
+        :chatusers="chatusers"
+      />
+    </div>
+    <div v-else-if="chatmessage.type === 'ReportedUser'">
+      <chat-message-report
+        :chat="chat"
+        :chatmessage="chatmessage"
+        :otheruser="otheruser"
+        :pov="pov"
+        :chatusers="chatusers"
+      />
+    </div>
     <div v-else-if="supportOrAdmin">
       Unknown chat message type {{ chatmessage.type }}
     </div>
