6761a2b82e4c87a325b427037d5f78044b92682a
edwh <edward@ehibbert.org.uk>
2021-06-20 13:26:14 +0100

    Proof of concept

--- components/Breakpoint.vue
+++ components/Breakpoint.vue
@@ -1,24 +1,39 @@
 <template>
   <span>
-    <span class="d-block d-sm-none">
-      XS
-    </span>
-    <span class="d-none d-sm-block d-md-none">
-      SM
-    </span>
-    <span class="d-none d-md-block d-lg-none">
-      MD
-    </span>
-    <span class="d-none d-lg-block d-xl-none">
-      LG
-    </span>
-    <span class="d-none d-xl-block">
-      XL
-    </span>
+    <span ref="xs" class="d-block d-sm-none" />
+    <span ref="sm" class="d-none d-sm-block d-md-none" />
+    <span ref="md" class="d-none d-md-block d-lg-none" />
+    <span ref="lg" class="d-none d-lg-block d-xl-none" />
+    <span ref="xl" class="d-none d-xl-block" />
   </span>
 </template>
 <script>
 export default {
-  props: {}
+  mounted() {
+    this.check()
+  },
+  methods: {
+    check() {
+      const oldBreakpoint = this.$store.getters['misc/get']('breakpoint')
+      let currentBreakpoint = oldBreakpoint
+      ;['xs', 'sm', 'md', 'ld', 'xl'].forEach(breakpoint => {
+        if (this.$refs[breakpoint]) {
+          if (getComputedStyle(this.$refs[breakpoint]).display === 'block') {
+            // This breakpoint is visible and is therefore the current one.
+            currentBreakpoint = breakpoint
+          }
+        }
+
+        if (currentBreakpoint !== oldBreakpoint) {
+          this.$store.dispatch('misc/set', {
+            key: 'breakpoint',
+            value: currentBreakpoint
+          })
+        }
+      })
+
+      setTimeout(this.check, 100)
+    }
+  }
 }
 </script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -19,6 +19,7 @@
       <div class="d-none">
         <ChatButton v-if="replyToSend" ref="replyToPostChatButton" :userid="replyToUser" />
       </div>
+      <Breakpoint />
     </client-only>
   </div>
 </template>
@@ -29,6 +30,7 @@ import replyToPost from '@/mixins/replyToPost'
 import LocalStorageMonitor from '~/components/LocalStorageMonitor'
 import BouncingEmail from '~/components/BouncingEmail'
 import MainHeader from '~/components/MainHeader'
+import Breakpoint from '~/components/Breakpoint'
 
 const ChatPopups = () => import('~/components/ChatPopups')
 const ChatButton = () => import('~/components/ChatButton')
@@ -36,6 +38,7 @@ const ExternalLink = () => import('~/components/ExternalLink')
 
 export default {
   components: {
+    Breakpoint,
     ChatPopups,
     ChatButton,
     LocalStorageMonitor,
