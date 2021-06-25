6dc0c7298ddc30404560cd592141f6698e803205
Edward Hibbert <edward@ehibbert.org.uk>
2021-06-25 11:50:57 +0100

    NPM build needs more space

--- setvars.cmd
+++ setvars.cmd
@@ -1,4 +1,5 @@
 set path=%HOMEDRIVE%%HOMEPATH%\AppData\Roaming\nvm\v12.13.1\;%path%
 set IZNIK_API=https://fdapidbg.ilovefreegle.org
 set API=http://fdapidbg.ilovefreegle.org/api
+set NODE_OPTIONS=--max_old_space_size=6000
 
