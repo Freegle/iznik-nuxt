e0c69b8b8532d111f3b9ece9190e5361b8bfd9ff
Edward Hibbert <edward@ehibbert.org.uk>
2021-08-02 08:40:09 +0100

    MT: Tweak repost help message.

--- components/ModSettingsGroup.vue
+++ components/ModSettingsGroup.vue
@@ -322,14 +322,14 @@
               :groupid="groupid"
               name="settings.reposts.offer"
               label="OFFER repost time"
-              description="Controls when the member can manually repost, and when auto-repost kicks in."
+              description="Controls when the member can manually repost, and when auto-repost kicks in.  0 = always show manual Repost button."
               class="mr-2"
             />
             <ModGroupSetting
               :groupid="groupid"
               name="settings.reposts.wanted"
               label="WANTED repost time"
-              description="Controls when the member can manually repost, and when auto-repost kicks in."
+              description="Controls when the member can manually repost, and when auto-repost kicks in.  0 = always show manual Repost button."
               class="mr-2"
             />
           </b-card-body>
