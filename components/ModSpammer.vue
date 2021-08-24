bcb35ee942faaa20308c717478c0c0a74defb48a
Edward Hibbert <edward@ehibbert.org.uk>
2021-08-24 16:30:43 +0100

    MT: Show other users active on the same IP as a spammer.

--- components/ModSpammer.vue
+++ components/ModSpammer.vue
@@ -1,27 +1,40 @@
 <template>
-  <NoticeMessage :variant="variant" class="mb-1">
-    <div>
-      {{ collname }}: {{ user.spammer.reason }}
-    </div>
-    <div class="small">
-      <span v-if="user.spammer.collection === 'PendingAdd'">
-        Reported by
-      </span>
-      <span v-else>
-        Added by
-      </span>
-      <span v-if="user.spammer.byuser">
-        {{ user.spammer.byuser.displayname }}
-        <span v-if="user.spammer.collection === 'PendingAdd' && hasPermissionSpamAdmin">
-          (<ExternalLink :href="'mailto:' + user.spammer.byuser.email + '?cc=spammerlist@ilovefreegle.org'">{{ user.spammer.byuser.email }}</ExternalLink>)
+  <div>
+    <NoticeMessage :variant="variant" class="mb-1">
+      <div>
+        {{ collname }}: {{ user.spammer.reason }}
+      </div>
+      <div class="small">
+        <span v-if="user.spammer.collection === 'PendingAdd'">
+          Reported by
         </span>
         <span v-else>
-          (<ExternalLink :href="'mailto:' + user.spammer.byuser.email">{{ user.spammer.byuser.email }}</ExternalLink>)
+          Added by
         </span>
-      </span>
-      #{{ user.spammer.byuserid }} {{ user.spammer.added | timeago }}
-    </div>
-  </NoticeMessage>
+        <span v-if="user.spammer.byuser">
+          {{ user.spammer.byuser.displayname }}
+          <span v-if="user.spammer.collection === 'PendingAdd' && hasPermissionSpamAdmin">
+            (<ExternalLink :href="'mailto:' + user.spammer.byuser.email + '?cc=spammerlist@ilovefreegle.org'">{{ user.spammer.byuser.email }}</ExternalLink>)
+          </span>
+          <span v-else>
+            (<ExternalLink :href="'mailto:' + user.spammer.byuser.email">{{ user.spammer.byuser.email }}</ExternalLink>)
+          </span>
+        </span>
+        #{{ user.spammer.byuserid }} {{ user.spammer.added | timeago }}
+      </div>
+    </NoticeMessage>
+    <notice-message v-if="sameip && sameip.length" variant="warning">
+      <p>
+        Recently active on the same IP address:
+        <span v-for="userid in sameip" :key="userid">
+          <v-icon name="hashtag" class="text-muted" scale="0.5" /><strong>{{ userid }}</strong>&nbsp;
+        </span>.
+      </p>
+      <p>
+        These may not be the same actual person but they're worth checking out too.
+      </p>
+    </notice-message>
+  </div>
 </template>
 <script>
 import NoticeMessage from './NoticeMessage'
@@ -32,6 +45,11 @@ export default {
     user: {
       type: Object,
       required: true
+    },
+    sameip: {
+      type: Array,
+      required: false,
+      default: null
     }
   },
   computed: {
