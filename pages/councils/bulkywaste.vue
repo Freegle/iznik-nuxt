397b393aaa384ed35a97fc2c315c073346779aa1
Edward Hibbert <edward@ehibbert.org.uk>
2021-10-17 14:39:54 +0100

    Add Freegle Focus on bulky waste page

---
+++ pages/councils/bulkywaste.vue
@@ -0,0 +1,242 @@
+<template>
+  <b-row class="m-0">
+    <b-col cols="12" lg="6" offset-lg="3" class="bg-white">
+      <b-card no-body variant="success">
+        <b-card-header>
+          <h1>
+            Freegle Focus: Bulky Waste - making room for reuse
+          </h1>
+        </b-card-header>
+        <b-card-text class="pl-2 pr-2">
+          <h3>
+            Bulky Waste, it's everyone's issue and the tonnages are big.
+          </h3>
+          <p>
+            From fridges and futons to televisions and tumble dryers, every local authority deals with bulky waste and there's a mountain of it.
+          </p>
+          <h3>
+            A large proportion of bulky waste is furniture.
+          </h3>
+          <p>
+            <!-- eslint-disable-next-line -->
+            <ExternalLink href="https://preprod.wrap.org.uk/sites/default/files/2020-09/WRAP-UK%20bulky%20waste%20summary_0.pdf">Research by WRAP</ExternalLink>
+            in 2012 estimated that 42% of all bulky waste in the UK is furniture, 19% is textiles (such as mattresses) and 19% is large waste electrical and electronic equipment (WEEE).
+          </p>
+          <h3>
+            More than half of all bulky waste items sent for disposal <em>could be reused</em>.
+          </h3>
+          <p>
+            The same research estimated that 32% of bulky items (across all items) taken to an HWRC were reusable in their current condition but this figure increased to 51% reusable if including items that required a slight repair (but were otherwise in good condition).
+          </p>
+          <h3>
+            Millions of families in the UK are living in Furniture Poverty.
+          </h3>
+          <p>
+            Claire Donovan, Head of Policy, Research & Campaigns at End Furniture Poverty explains:-
+          </p>
+          <p>
+            <span class="quote">Millions of families are living in Furniture Poverty across the UK, unable to afford to buy or replace essential furniture items, items which local authorities are collecting through bulky household waste contracts and then, in many cases, disposing of. If local authorities take good quality, preloved furniture items out of the waste stream, they can be used to help families in a time of need, and potentially ease pressures on other frontline services.</span>
+          </p>
+          <h3>
+            From reusable to unusable.
+          </h3>
+          <p>
+            With this in mind, why does so much bulky waste still get sent for disposal?
+          </p>
+          <p>
+            In short, it’s a quick and easy option and residents are often unaware of the alternatives.<a href="#foot1"><sup>[1]</sup></a> Research by WRAP has shown that people do not actively seek information about reuse, and rely instead on what they already know or have done in the past.
+            </a>
+          </p>
+          <p>
+            For residents who choose to take their bulky items to the HWRC, disposal is straightforward, quick and often free-of-charge. For those who utilise a bulky waste collection service, there’s the additional convenience of not having to move or transport the item. The cost is affordable and in some cases, free-of-charge. In both scenarios, bulky items quickly go from being reusable to unusable.
+          </p>
+          <h3>
+            ChooseToReuse - A simple act with far reaching benefits.
+          </h3>
+          <p>
+            When bulky household items are diverted into reuse there are a multitude of well documented benefits, including:-
+          </p>
+          <ul>
+            <li>Creating a circular economy.</li>
+            <li>Reducing waste & the associated collection / disposal costs for local authorities and residents.</li>
+            <li>Encouraging sustainability.</li>
+            <li>Protecting resources.</li>
+            <li>Reducing consumption-based GHG emissions.<a href="#foot2"><sup>[2]</sup></a></li>
+            <li>Reducing furniture poverty.<a href="#foot3"><sup>[3]</sup></a></li>
+          </ul>
+          <h3>
+            Partnerships are Key to Success.
+          </h3>
+          <p>
+            WRAP’s
+            <!-- eslint-disable-next-line -->
+            <ExternalLink href="https://wrap.org.uk/resources/guide/re-use/partnerships-re-use">PAKTS</ExternalLink>
+            (Partnerships are Key To Success) initiative encourages local authorities to work with existing organisations, including charities, to encourage reuse. A suite of resources are available including how-to guides and case studies, all highlighting the importance of reuse and partnership working.
+          </p>
+          <p>
+            There are a number of tried and tested ways in which councils can collaborate with external organisations on reuse.
+            Examples include the introduction of charity-led reuse shops at HWRCs (for example in
+            <!-- eslint-disable-next-line -->
+            <ExternalLink href="https://wrap.org.uk/resources/case-study/bulky-waste/buckinghamshire-case-study">Buckinghamshire</ExternalLink>)
+            and employing charities or social enterprise to run the bulky waste collection service (for example
+            <!-- eslint-disable-next-line -->
+            <ExternalLink href="https://liverpool.gov.uk/bins-and-recycling/bulky-item-collection/">Bulky Bob’s for Liverpool City Council</ExternalLink>).
+            Clearly these initiatives are fantastic, best practice examples with hugely positive impacts on local communities, but this kind of
+            partnership has rarely been achieved to date. The time, resources and infrastructure required is significant and for many councils this cannot happen overnight.
+          </p>
+          <p>
+            For local authorities, we are able to provide a range of tools, resources and reuse data, specific to your area. Find out more about how
+            <!-- eslint-disable-next-line -->
+            <nuxt-link to="/councils/why">Freegle works with councils</nuxt-link>.
+          </p>
+          <p>
+            <span class="quote">We urge all local authorities to look at their bulky waste stream, start working with furniture reuse charities and organisations like Freegle, and help to End Furniture Poverty.</span>
+            <br>
+            Claire Donovan, Head of Policy, Research & Campaigns at End Furniture Poverty
+          </p>
+          <h3>
+            Make room for reuse.
+          </h3>
+          <p>
+            Whether you currently offer a free-of-charge bulky waste pick up; provide a chargeable service; operate in-house or via contractor, we understand the complexities, commitments and financial constraints that may be involved with the ongoing provision of these services.
+          </p>
+          <p>
+            At Freegle, we believe there is always ‘room for reuse’. Across the UK, our self-sufficient and highly resourceful Freegle communities are already demonstrating a fantastic amount of reuse, often with very little external input required.
+          </p>
+          <p>
+            It couldn’t be easier to get involved as Michael Singham, Waste Strategy Manager at Richmond and Wandsworth Councils describes:-
+          </p>
+          <p>
+            <span class="quote">Wandsworth Freegle and Richmond-upon-Thames Freegle are two extremely active groups operating across these boroughs. Every tonne of bulky waste diverted from disposal by this freegling activity saves between £120-160 in disposal costs.  Local residents initiated the groups, Freegle set them up and all we had to do was link to them on the bulky waste pages of the Councils’ web sites.  The local group memberships snowballed from there.  The tonnages reused over the last 12 months by these two communities combined is 149 tonnes with associated disposal cost savings of between £17,880 & £23,840.</span>
+          </p>
+          <p>
+            A mutually beneficial partnership which facilitates more reuse in any community is a win-win situation. Freegle has formed partnerships with a growing number of local authorities - for some of which case studies are available -
+            <!-- eslint-disable-next-line -->
+            <nuxt-link to="/councils/casestudies/cumbria">Cumbria County Council</nuxt-link> and <nuxt-link to="/councils/casestudies/essex">Essex County Council</nuxt-link>.
+          </p>
+          <p id="foot1">
+            [1] A Survey in 2018 by NLWA that looked at what people do with their broken or damaged furniture, found that for 23 per cent of respondents, disposal was ‘the easiest thing to do’, another 23 per cent said ‘they weren’t sure how to dispose of these items’, whilst 20 per cent said ‘it was cheaper and easier to replace items rather than repair them’.
+          </p>
+          <p id="foot2">
+            [2] WRAP study:
+            <!-- eslint-disable-next-line -->
+            <ExternalLink href="https://preprod.wrap.org.uk/sites/default/files/2020-09/WRAP-Domestic%20Furniture%20chapter_final_1.pdf">Benefits of Reuse Case Study: Domestic Furniture</ExternalLink>
+            highlights the beneficial impacts which result from the reuse of domestic furniture - which makes up 42% of the U.K.’s bulky waste.
+            The study found that providing 1 tonne of sofas for direct reuse can result in a net GHG saving of 1.45 tonnes CO2-eq. This is approximately 55kg CO2-eq per sofa.
+          </p>
+          <p id="foot3">
+            [3] Fallout from the pandemic has created financial instability for many:-
+          </p>
+          <ul>
+            <li>Almost half a million UK families were in rent arrears in early 2020, according to the Resolution Foundation;</li>
+            <li>Budget cuts mean one in four people in England can no longer apply for essential furniture items from their local authority, according to research published by End Furniture Poverty.</li>
+            <li>Millions of families across the U.K. are living in Furniture Poverty. Claire Donovan, Head of Policy, Research & Campaigns at End Furniture Poverty commented for this Freegle Focus report “Living in Furniture Poverty, without a bed to sleep on, a sofa to sit on or a cooker to prepare meals, can have a devastating impact on people’s mental and physical health and their social wellbeing. Helping a family out of Furniture Poverty not only improves their quality of life but can stop them getting into unmanageable debt to obtain furniture themselves which can leave people unable to buy food or pay their rent.”</li>
+          </ul>
+          <h3>
+            Are you ready to make room for reuse? Here are 6 quick ways to make a difference today:-
+          </h3>
+          <ol>
+            <li>
+              Add a link to Freegle on your website’s waste pages.
+              <!-- eslint-disable-next-line -->
+              <nuxt-link to="/councils/workbest#makingvisible">Make Freegle visible</nuxt-link>.
+            </li>
+            <li>
+              Use a pre-recorded ‘hold time’ message in your call centre to let residents know about the alternative options for reuse. We have an
+              <!-- eslint-disable-next-line -->
+              <nuxt-link to="/councils/workbest#bulkywaste">example telephone script</nuxt-link>
+              you could base this on. You could also consider adding options for reuse to your outgoing auto response emails and to the script used by call centre staff.
+            </li>
+            <li>
+              Follow us on
+              <!-- eslint-disable-next-line -->
+              <ExternalLink href="https://www.facebook.com/Freegle">Facebook</ExternalLink> and <ExternalLink href="https://twitter.com/thisisfreegle">Twitter</ExternalLink>.
+              Share our reuse ideas and inspire your own community.
+            </li>
+            <li>
+              Sign up for
+              <!-- eslint-disable-next-line -->
+              <ExternalLink href="https://us20.campaign-archive.com/home/?u=dd0a44d9aa3f6ba0e4d439318&id=8edc47538e">Freegle Bites</ExternalLink>,
+              our e-newsletter for councils, which is packed with reuse news and ways to get involved.
+            </li>
+            <li>
+              Find your
+              <!-- eslint-disable-next-line -->
+              <ExternalLink href="https://www.ilovefreegle.org/explore">local Freegle communities</ExternalLink>
+              using our search and link to them from your website.
+            </li>
+            <li>
+              Find out how many tonnes of waste (& associated GHG savings) your local Freegle communities have
+              successfully diverted away from the waste stream and into reuse during the last year using our
+              <!-- eslint-disable-next-line -->
+              <nuxt-link to="/stats/authorities">live stats tool</nuxt-link>.
+              And celebrate this!
+            </li>
+          </ol>
+          <p>
+            Freegle is here, ready to help any council who wants to improve on reuse. We can provide support, advice, meaningful data and much more besides.
+            Contact us to find out more at
+            <!-- eslint-disable-next-line -->
+            <ExternalLink href="mailto:councils@ilovefreegle.org">councils@ilovefreegle.org</ExternalLink>.
+          </p>
+          <p>
+            <!-- eslint-disable-next-line -->
+            <nuxt-link to="/councils">Access our Communications Toolkit for Councils</nuxt-link>
+          </p>
+          <h3>
+            Further information
+          </h3>
+          <p>
+            Freegle is a national online reuse platform and easy-to-use app. Freegle has been busy facilitating reuse
+            in the community since 2009. Our network of local communities, run entirely by volunteers, help to make
+            reuse simple and accessible to all. The best part about Freegle is that it is free to use and everything
+            on Freegle, is free! <ExternalLink href="https://www.ilovefreegle.org">
+              www.ilovefreegle.org
+            </ExternalLink>
+          </p>
+        </b-card-text>
+      </b-card>
+    </b-col>
+    <b-col>
+      <CouncilsContent />
+    </b-col>
+  </b-row>
+</template>
+<script>
+import buildHead from '@/mixins/buildHead.js'
+import sectionScroll from '@/mixins/sectionScroll.js'
+import CouncilsContent from '../../components/CouncilsContent'
+const ExternalLink = () => import('~/components/ExternalLink')
+
+export default {
+  components: { CouncilsContent, ExternalLink },
+  mixins: [buildHead, sectionScroll],
+  head() {
+    return this.buildHead(
+      'Councils - Banners',
+      'Freegle has artwork for banners that you can print'
+    )
+  }
+}
+</script>
+<style scoped lang="scss">
+.anchor {
+  display: block;
+  position: relative;
+  top: -68px;
+  visibility: hidden;
+}
+.quote {
+  font-size: 1.25rem;
+  font-weight: bold;
+  font-style: italic;
+
+  &::before {
+    content: open-quote;
+  }
+
+  &::after {
+    content: close-quote;
+  }
+}
+</style>
