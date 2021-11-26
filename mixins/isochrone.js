e961921d6934107c963b414a99949631fe1bf3e8
Edward Hibbert <edward@ehibbert.org.uk>
2021-11-26 10:23:51 +0000

    Isochrone fixes

--- mixins/isochrone.js
+++ mixins/isochrone.js
@@ -9,46 +9,37 @@ if (process.browser) {
 export default {
   computed: {
     isochroneBounds() {
+      // We have a problem with getting the bounds using leaflet - it looks like a timing error.  So do it ourselves.
       const isochrones = Object.values(this.$store.getters['isochrones/list'])
       console.log('Isochrones', isochrones)
       let bounds = null
 
-      if (L && isochrones && isochrones.length) {
-        if (isochrones.length === 1) {
+      if (isochrones && isochrones.length) {
+        let swlat = null
+        let swlng = null
+        let nelat = null
+        let nelng = null
+
+        isochrones.forEach(i => {
           try {
             const wkt = new Wkt.Wkt()
-            console.log('Add single wkt', wkt)
-            wkt.read(isochrones[0].polygon)
+            wkt.read(i.polygon)
             const obj = wkt.toObject()
-            bounds = obj.getBounds().pad(0.1)
+            const thisbounds = obj.getBounds()
+            const thissw = thisbounds.getSouthWest()
+            const thisne = thisbounds.getNorthEast()
+
+            swlat = swlat === null ? thissw.lat : Math.min(swlat, thissw.lat)
+            swlng = swlng === null ? thissw.lng : Math.min(swlng, thissw.lng)
+            nelat = nelat === null ? thisne.lat : Math.max(nelat, thisne.lat)
+            nelng = nelng === null ? thisne.lng : Math.min(nelng, thisne.lng)
           } catch (e) {
             console.log('WKT error', location, e)
           }
-        } else {
-          // eslint-disable-next-line new-cap
-          const fg = new L.featureGroup()
+        })
 
-          Object.values(isochrones).forEach(i => {
-            try {
-              const wkt = new Wkt.Wkt()
-              console.log('Add wkt', wkt)
-              wkt.read(i.polygon)
-              const obj = wkt.toObject()
-              fg.addLayer(obj)
-            } catch (e) {
-              console.log('WKT error', location, e)
-            }
-          })
-
-          console.log('Feature group', fg)
-          console.log('Layers', fg.getLayers())
-          console.log('Layers length', fg.getLayers().length)
-
-          if (fg.getLayers().length) {
-            bounds = fg.getBounds()
-            console.log('Bounds', bounds)
-            bounds = bounds.pad(0.1)
-          }
+        if (swlat !== null) {
+          bounds = new L.LatLngBounds([[swlat, swlng], [nelat, nelng]])
         }
       }
 
