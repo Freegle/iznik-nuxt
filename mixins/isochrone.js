f31e9f2cea461b729ad0e5d09cc24123b76c96fa
Edward Hibbert <edward@ehibbert.org.uk>
2021-11-26 10:10:34 +0000

    Isochrone fixes

--- mixins/isochrone.js
+++ mixins/isochrone.js
@@ -10,28 +10,45 @@ export default {
   computed: {
     isochroneBounds() {
       const isochrones = Object.values(this.$store.getters['isochrones/list'])
+      console.log('Isochrones', isochrones)
       let bounds = null
 
       if (L && isochrones && isochrones.length) {
-        // eslint-disable-next-line new-cap
-        const fg = new L.featureGroup()
-
-        Object.values(isochrones).forEach(i => {
+        if (isochrones.length === 1) {
           try {
             const wkt = new Wkt.Wkt()
-            wkt.read(i.polygon)
+            console.log('Add single wkt', wkt)
+            wkt.read(isochrones[0].polygon)
             const obj = wkt.toObject()
-            fg.addLayer(obj)
+            bounds = obj.getBounds().pad(0.1)
           } catch (e) {
             console.log('WKT error', location, e)
           }
-        })
+        } else {
+          // eslint-disable-next-line new-cap
+          const fg = new L.featureGroup()
 
-        console.log('Layers', fg.getLayers())
-        console.log('Layers length', fg.getLayers().length)
+          Object.values(isochrones).forEach(i => {
+            try {
+              const wkt = new Wkt.Wkt()
+              console.log('Add wkt', wkt)
+              wkt.read(i.polygon)
+              const obj = wkt.toObject()
+              fg.addLayer(obj)
+            } catch (e) {
+              console.log('WKT error', location, e)
+            }
+          })
 
-        if (fg.getLayers().length) {
-          bounds = fg.getBounds().pad(0.1)
+          console.log('Feature group', fg)
+          console.log('Layers', fg.getLayers())
+          console.log('Layers length', fg.getLayers().length)
+
+          if (fg.getLayers().length) {
+            bounds = fg.getBounds()
+            console.log('Bounds', bounds)
+            bounds = bounds.pad(0.1)
+          }
         }
       }
 
