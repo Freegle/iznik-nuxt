43e3426e82502988fe488e3d99d904075002b026
Edward Hibbert <edward@ehibbert.org.uk>
2022-02-14 13:50:43 +0000

    Fix SSR error on Help page.

--- components/TermsOfUse.vue
+++ components/TermsOfUse.vue
@@ -11,18 +11,16 @@
       <li>Everything must be free and legal.</li>
       <li>Be nice to other freeglers, and they'll be nice to you.</li>
       <li>
-        We don't see or check items offered - see our <nuxt-link to="/disclaimer">
-          disclaimer.
-        </nuxt-link>
+        <!-- eslint-disable-next-line-->
+        We don't see or check items offered - see our <nuxt-link to="/disclaimer">disclaimer</nuxt-link>.
       </li>
       <li>
         Freegle communities may have additional rules.  If you have questions, scroll down and use the
         <em>Contact community volunteers</em> button.
       </li>
       <li>
-        Please <nuxt-link to="/disclaimer">
-          keep yourself safe and watch out for scams.
-        </nuxt-link>
+        <!-- eslint-disable-next-line-->
+        Please <nuxt-link to="/disclaimer">keep yourself safe and watch out for scams</nuxt-link>.
       </li>
       <li>Objectionable content or behaviour will not be tolerated.</li>
       <li>
@@ -32,19 +30,16 @@
       </li>
       <li>You must be aged 13 or over, as there is user-generated content.</li>
       <li>
-        We won’t sell your email address or personal data to anyone else - see <nuxt-link to="/privacy">
-          privacy page.
-        </nuxt-link>
-      </li>
-      <li>
-        We will email you posts, replies, events, newsletters, etc. - control these in
         <!-- eslint-disable-next-line-->
-        <nuxt-link to="/settings">Settings</nuxt-link>.
+        We won’t sell your email address or personal data to anyone else - see <nuxt-link to="/privacy">privacy page</nuxt-link>.
       </li>
       <li>
-        We will make your post public to publicise Freegle, but not your email address - see <nuxt-link to="/privacy">
-          privacy page.
-        </nuxt-link>
+        <!-- eslint-disable-next-line-->
+        We will email you posts, replies, events, newsletters, etc. - control these in <nuxt-link to="/settings">Settings</nuxt-link>.
+      </li>
+      <li>
+        <!-- eslint-disable-next-line-->
+        We will make your post public to publicise Freegle, but not your email address - see <nuxt-link to="/privacy">privacy page</nuxt-link>.
       </li>
     </ol>
   </div>
