23e669ce10eec66bc72edece7650552734df9eca
Edward Hibbert <edward@ehibbert.org.uk>
2022-03-04 14:03:25 +0000

    Update ZoomStock time.

--- components/ModZoomStock.vue
+++ components/ModZoomStock.vue
@@ -1,25 +1,24 @@
 <template>
   <div class="font-weight-bold">
-    <div v-if="today" class="pulsate d-flex">
-      <!-- eslint-disable-next-line-->
-      <ExternalLink href="https://us02web.zoom.us/j/81013447754?pwd=cVYzeEl5dWx0MEd0TTdOWE9Yd0Z4QT09" class="text-white">ZoomStock Thursdays - join other volunteers for a natter.  Click here at 2pm.</ExternalLink>
-    </div>
-    <div v-else-if="now" class="pulsate d-flex">
+    <div v-if="happening" class="pulsate d-flex">
       <!-- eslint-disable-next-line-->
       <ExternalLink href="https://us02web.zoom.us/j/81013447754?pwd=cVYzeEl5dWx0MEd0TTdOWE9Yd0Z4QT09" class="text-white">ZoomStock happening now! Join other volunteers for a natter - click here.</ExternalLink>
     </div>
+    <div v-else-if="today" class="pulsate d-flex">
+      <!-- eslint-disable-next-line-->
+      <ExternalLink href="https://us02web.zoom.us/j/81013447754?pwd=cVYzeEl5dWx0MEd0TTdOWE9Yd0Z4QT09" class="text-white">ZoomStock Thursdays - join other volunteers for a natter.  Click here at 2pm.</ExternalLink>
+    </div>
     <div v-else class="d-flex">
-      ZoomStock Thursdays @ 2pm, next {{ timeago(fromNow) }}. Join other volunteers for a natter.  Link will be here.
+      ZoomStock Tuesdays/Thursdays 1-5pm. Join other volunteers for a natter.  Link will be here.
     </div>
   </div>
 </template>
 <script>
 import ExternalLink from '@/components/ExternalLink'
 
-const DAY_OF_WEEK = 4
-const HOUR_OF_DAY = 14
-const MINUTE_START = 0
-const MINUTE_END = 40
+const DAY_OF_WEEK = [2, 4]
+const HOUR_OF_DAY = 13
+const DURATION = 240
 
 export default {
   components: { ExternalLink },
@@ -29,40 +28,42 @@ export default {
     }
   },
   computed: {
-    fromNow() {
-      return this.$store.getters['misc/time'] && this.nextOne
-        ? this.nextOne
-        : null
+    now() {
+      if (this.$store.getters['misc/time']) {
+        return this.$dayjs()
+      }
+
+      return this.$dayjs
     },
     today() {
-      const d = this.$dayjs()
+      const d = this.now
       return (
         this.$store.getters['misc/time'] &&
-        d.day() === DAY_OF_WEEK &&
+        DAY_OF_WEEK.indexOf(d.day()) !== -1 &&
         d.hour() < HOUR_OF_DAY
       )
     },
-    now() {
-      const d = this.$dayjs()
-      return (
-        this.$store.getters['misc/time'] &&
-        d.day() === DAY_OF_WEEK &&
-        d.hour() === HOUR_OF_DAY &&
-        d.minute() >= MINUTE_START &&
-        d.minute() <= MINUTE_END
-      )
-    }
-  },
-  mounted() {
-    let d = this.$dayjs()
+    happening() {
+      const d = this.now
 
-    if (d.day() < DAY_OF_WEEK) {
-      d = d.day(DAY_OF_WEEK)
-    } else {
-      d = d.add(1, 'week').day(DAY_OF_WEEK)
-    }
+      if (DAY_OF_WEEK.indexOf(d.day()) === -1) {
+        // Not today.
+        return false
+      }
 
-    this.nextOne = d
+      if (d.hour() < HOUR_OF_DAY) {
+        // Not started
+        return false
+      }
+
+      const end = this.now.hour(HOUR_OF_DAY).add(DURATION, 'minute')
+
+      if (this.now.isBefore(end)) {
+        return true
+      }
+
+      return false
+    }
   }
 }
 </script>
