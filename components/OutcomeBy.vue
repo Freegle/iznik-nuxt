a6a4d16d12136ee7ef0ee9c23b2c76475866e048
edwh <edward@ehibbert.org.uk>
2022-03-22 11:25:23 +0000

    Duplicate user showing up for marking as taken

--- components/OutcomeBy.vue
+++ components/OutcomeBy.vue
@@ -149,25 +149,6 @@ export default {
         u => !this.selectedUsers.find(u2 => u2.userid === u.userid)
       )
 
-      const retids = ret.map(r => r.userid)
-
-      // Now add any other users that we have chatted to.
-      const chats = this.$store.getters['chats/list']
-
-      chats.forEach(c => {
-        if (c.chattype === 'User2User' && c.user1 && c.user2) {
-          const otheruser = c.user1.id === this.myid ? c.user2 : c.user1
-          if (!retids[otheruser.id]) {
-            ret.push({
-              userid: otheruser.id,
-              displayname: otheruser.displayname
-            })
-
-            retids[otheruser.id] = true
-          }
-        }
-      })
-
       return ret
     },
     moreUsersToSelect() {
