00937963a9f28ab0b8f691237be9ee8036fd9a3a
Edward Hibbert <edward@ehibbert.org.uk>
2022-04-19 10:15:40 +0100

    Improve image carousel arrows

--- components/ImageCarousel.vue
+++ components/ImageCarousel.vue
@@ -57,12 +57,4 @@ export default {
   max-height: calc(100vh - 150px) !important;
   object-fit: cover;
 }
-
-/deep/ .carousel-control-prev-icon {
-  background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%2380808' viewBox='0 0 8 8'%3E%3Cpath d='M5.25 0l-4 4 4 4 1.5-1.5-2.5-2.5 2.5-2.5-1.5-1.5z'/%3E%3C/svg%3E") !important;
-}
-
-/deep/ .carousel-control-next-icon {
-  background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%2380808' viewBox='0 0 8 8'%3E%3Cpath d='M2.75 0l-1.5 1.5 2.5 2.5-2.5 2.5 1.5 1.5 4-4-4-4z'/%3E%3C/svg%3E") !important;
-}
 </style>
