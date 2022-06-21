d3904e8d4b05921967bc448d87aaac7f42e50e48
Edward Hibbert <edward@ehibbert.org.uk>
2022-06-21 16:54:19 +0100

    MT: Error on Volunteering page

--- pages/modtools/volunteering/_id.vue
+++ pages/modtools/volunteering/_id.vue
@@ -101,17 +101,6 @@ export default {
           })
       }
     }
-  },
-  head() {
-    if (this.invalid || !this.volunteering) {
-      return this.buildHead('Volunter Opportunity #' + this.id)
-    } else {
-      return this.buildHead(
-        this.volunteering.title,
-        this.volunteering.description,
-        null
-      )
-    }
   }
 }
 </script>
