ff38d2732637a3ab3925c8176de220c6314e4bd3
Edward Hibbert <edward@ehibbert.org.uk>
2023-03-13 16:30:38 +0000

    Change Bytemark logo to Krystal

--- components/MainFooter.vue
+++ components/MainFooter.vue
@@ -2,16 +2,15 @@
   <div class="footer w-100 mt-4">
     <div class="d-flex flex-column">
       <div class="d-flex justify-content-center mb-2">
-        <a target="_blank" rel="noopener" href="https://www.bytemark.co.uk/company/case-studies/freegle/" class="d-flex justify-content-start flex-column mr-4">
-          <img src="/bytemark.png" alt="Bytemark Logo" class="sponsor">
-        </a>
+        <a target="_blank" rel="noopener" href="https://affiliate.k.io/go/Freegle-Krystal" class="d-flex justify-content-start flex-column mr-4">
+          <b-img src="/krystal.png" alt="Krystal Logo" class="sponsor" /></a>
         <a target="_blank" rel="noopener" href="https://www.mythic-beasts.com" class="d-flex justify-content-start flex-column ml-4">
           <img src="/mythic-beasts.png" alt="Mythic Beasts Logo" class="sponsor">
         </a>
       </div>
       <div class="d-flex flex-column justify-content-end small mb-2">
         <div class="text-muted text-center mb-1">
-          Freegle is registered as a charity with HMRC (ref. XT32865). Kind support from Bytemark & Mythic Beasts.
+          Freegle is registered as a charity with HMRC (ref. XT32865). Kind support from Krystal & Mythic Beasts.
         </div>
       </div>
       <div class="d-flex justify-content-between navlinks">
