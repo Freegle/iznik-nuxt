672f411c4d40c0994516f6121abfbd0ad82579e8
Edward Hibbert <edward@ehibbert.org.uk>
2023-03-16 16:06:07 +0000

    MT: Stop ZoomStock link flashing.

--- components/ModZoomStock.vue
+++ components/ModZoomStock.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="font-weight-bold">
-    <div v-if="now" class="text-danger d-flex">
+    <div v-if="now" class="d-flex">
       <!-- eslint-disable-next-line-->
-      <ExternalLink href="https://zoom.us/j/95789187203?pwd=VE9Va1M5YWJWY0M1dC9sc014L0xjQT09" :class="colorClass">ZoomStock happening now till 5pm! Join other volunteers for a natter - click here.</ExternalLink>
+      <ExternalLink href="https://zoom.us/j/95789187203?pwd=VE9Va1M5YWJWY0M1dC9sc014L0xjQT09" class="text-warning">ZoomStock happening now till 5pm! Join other volunteers for a natter - click here.</ExternalLink>
     </div>
-    <div v-else-if="today" class="text-danger d-flex">
+    <div v-else-if="today" class="d-flex">
       <!-- eslint-disable-next-line-->
       <ExternalLink href="https://zoom.us/j/95789187203?pwd=VE9Va1M5YWJWY0M1dC9sc014L0xjQT09" :class="colorClass">ZoomStock Thursdays - join other volunteers for a natter.  Link will be here 2pm-5pm.</ExternalLink>
     </div>
