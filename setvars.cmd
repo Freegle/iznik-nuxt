380fc65bd2c023e5b3fbd000b652bd1c27c77763
Edward Hibbert <edward@ehibbert.org.uk>
2024-11-19 08:48:34 +0000

    Facebook app debugging.

--- setvars.cmd
+++ setvars.cmd
@@ -2,4 +2,4 @@ set path=%HOMEDRIVE%%HOMEPATH%\AppData\Roaming\nvm\v12.13.1\;%path%
 set IZNIK_API=https://fdapidbg.ilovefreegle.org
 set API=http://fdapidbg.ilovefreegle.org/api
 set NODE_OPTIONS=--max_old_space_size=6000
-
+set FACEBOOK_APP_ID=134980666550322
