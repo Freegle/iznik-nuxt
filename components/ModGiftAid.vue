9ca68b4a6e6a7879fc7aa0807e09836503ba833e
Edward Hibbert <edward@ehibbert.org.uk>
2024-11-30 12:37:33 +0000

    MT: Add user id and gift aid id to Gift Aid display.

--- components/ModGiftAid.vue
+++ components/ModGiftAid.vue
@@ -16,7 +16,8 @@
           </NoticeMessage>
           <span class="small text-muted">
             {{ timeago(giftaid.timestamp) }}
-            <v-icon name="hashtag" class="text-muted" scale="0.75" />{{ giftaid.userid }}
+            User ID <v-icon name="hashtag" class="text-muted" scale="0.75" />{{ giftaid.userid }}
+            Gift Aid ID <v-icon name="hashtag" class="text-muted" scale="0.75" />{{ giftaid.id }}
           </span>
         </div>
       </b-col>
