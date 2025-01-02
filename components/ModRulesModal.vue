7c15180554d739f73bf44812d699449fde6f42b6
Edward Hibbert <edward@ehibbert.org.uk>
2025-01-02 14:22:11 +0000

    MT: Push for more rules completion.

--- components/ModRulesModal.vue
+++ components/ModRulesModal.vue
@@ -5,6 +5,9 @@
       v-model="showModal"
       size="lg"
       no-stacking
+      no-close-on-backdrop
+      no-close-on-esc
+      hide-header-close
     >
       <template slot="modal-title" class="w-100">
         Please configure group rules
@@ -13,8 +16,9 @@
         <ModMissingRules expanded />
       </template>
       <template slot="modal-footer" slot-scope="{ cancel }">
-        <b-button variant="secondary" @click="cancel">
+        <b-button variant="secondary" :disabled="enableIn > 0" @click="cancel">
           Not now
+          <span v-if="enableIn > 0">({{ enableIn }})</span>
         </b-button>
       </template>
     </b-modal>
@@ -28,11 +32,41 @@ export default {
   components: { ModMissingRules },
   mixins: [modal],
   data: function() {
-    return {}
+    return {
+      enableIn: 0,
+      timer: null
+    }
   },
   computed: {
     group() {
       return this.myGroup(this.groupid)
+    },
+    delay() {
+      const now = new Date()
+      const start = new Date(now.getFullYear(), 0, 0)
+      const diff = now - start
+      const oneDay = 1000 * 60 * 60 * 24
+      return Math.floor(diff / oneDay + 10)
+    }
+  },
+  mounted() {
+    this.enableIn = this.delay
+    this.timer = setTimeout(this.tick, 1000)
+  },
+  beforeDestroy() {
+    if (this.timer) {
+      clearTimeout(this.timer)
+    }
+  },
+  methods: {
+    tick() {
+      this.timer = null
+
+      if (this.enableIn > 0) {
+        this.enableIn -= 1
+
+        this.timer = setTimeout(this.tick, 1000)
+      }
     }
   }
 }
